0fd01e748c2e75d520b1f6d76a17716d67c56b4d
Harris Levine <acyclicgraphs@gmail.com>
2020-12-23 11:52:33 -0500

    Add core contracts to deployment

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -7,14 +7,46 @@ async function main() {
 
   console.log("Account balance:", (await signers[0].getBalance()).toString());
 
-  const tokenFactory = await ethers.getContractFactory('ZapToken')
+  const tokenFactory = await ethers.getContractFactory('ZapToken');
   const zapToken = await tokenFactory.deploy();
 
   const faucetContract = await ethers.getContractFactory('Faucet');
   const faucet = await faucetContract.deploy(zapToken.address);
 
+  // Core Contracts 
+
+  const coordinator = await ethers.getContractFactory('ZapCoordinator');
+  const Coordinator = await coordinator.deploy();
+
+  const database = await ethers.getContractFactory('Database')
+  const Database = await database.deploy();
+
+
+  // The majority of the core contracts take the Coordinator as params
+  const arbiter = await ethers.getContractFactory('Arbiter');
+  const Arbiter = await arbiter.deploy(Coordinator.address);
+
+  const bondage = await ethers.getContractFactory('Bondage');
+  const Bondage = await bondage.deploy(Coordinator.address);
+
+  const dispatch = await ethers.getContractFactory('Dispatch')
+  const Dispatch = await dispatch.deploy(Coordinator.address);
+  
+  const registry = await ethers.getContractFactory('Registry')
+  const Registry = await registry.deploy(Coordinator.address);
+
+
   console.log("zapToken address:", zapToken.address);
   console.log("Faucet address:", faucet.address);
+  console.log("ZapCoordinator address:", Coordinator.address);
+  console.log("Database address:", Database.address);
+  console.log("Arbiter address:", Arbiter.address);
+  console.log("Bondage address:", Bondage.address);
+  console.log("Registry address:", Registry.address);
+
+
+
+
 
 
 
