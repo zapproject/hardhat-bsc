f27763ddd42980d379a6f43d645105d8e1d73922
kimani kelly <kimanikelly@yahoo.com>
2020-12-28 00:09:48 -0500

    Funding accounts with test ZAP All accounts are able to receive test ZAP Issue with the test ZAP incrementing after each task execution

--- tasks/faucet.js
+++ tasks/faucet.js
@@ -1,5 +1,6 @@
 
 const { task, taskArgs } = require("hardhat/config");
+const { getTokenSourceMapRange } = require("typescript");
 require("hardhat-deploy-ethers");
 require('hardhat-deploy')
 
@@ -7,15 +8,46 @@ require('hardhat-deploy')
 task("faucet", "Sends 100K ZAP to the first 20 accounts")
     .setAction(async () => {
 
+        // Hardhat test accounts
         const signers = await ethers.getSigners();
 
+        // Connection to ZapToken.sol
         const Token = await ethers.getContractFactory('ZapToken')
         const token = await Token.attach('0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6');
 
+        // Connection to Faucet.sol
         const Faucet = await ethers.getContractFactory('Faucet');
         const faucet = await Faucet.attach('0x8A791620dd6260079BF849Dc5567aDC3F2FdC318')
 
-        console.log(faucet)
+
+        await token.allocate(faucet.address, 999999999)
+            .then((res) => {
+                return res
+            })
+            .catch((err) => {
+                return err
+            })
+
+        const balances = []
+
+        for (var i = 0; i < signers.length; i++) {
+
+
+            await faucet.buyZap(signers[i].address, 100)
+
+            balances.push(await token.balanceOf(signers[i].address))
+
+            console.log(
+                signers[i].address + ': ' +
+                parseInt(balances[i]._hex) + ' ZAP'
+
+            )
+        }
+
+
+
+
+
 
     });
 
