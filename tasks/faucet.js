43f209950ba838e12566cd5ab47e2212182d6589
kimani kelly <kimanikelly@yahoo.com>
2020-12-27 21:24:17 -0500

    Valid connection to the deployed Faucet.sol contract

--- tasks/faucet.js
+++ tasks/faucet.js
@@ -12,7 +12,10 @@ task("faucet", "Sends 100K ZAP to the first 20 accounts")
         const Token = await ethers.getContractFactory('ZapToken')
         const token = await Token.attach('0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6');
 
-        console.log(await token.symbol())
+        const Faucet = await ethers.getContractFactory('Faucet');
+        const faucet = await Faucet.attach('0x8A791620dd6260079BF849Dc5567aDC3F2FdC318')
+
+        console.log(faucet)
 
     });
 
