56822d842fb81579e0b127db2d6fd9d21ad6734e
kimani kelly <kimanikelly@yahoo.com>
2021-01-21 21:21:50 -0500

    Database contract connected to the Registry contract

--- test/RegistryTest.ts
+++ test/RegistryTest.ts
@@ -1,39 +1,91 @@
 import { ethers } from "hardhat";
 import { solidity } from "ethereum-waffle";
 import chai from "chai";
+import { Database } from "../typechain/Database";
 
 import { ZapCoordinator } from '../typechain/ZapCoordinator';
 
 import { Registry } from '../typechain/Registry';
 
+chai.use(solidity);
+
+const { expect } = chai;
 
 describe('Registry Test', () => {
+
+    let database: Database;
     let coordinator: ZapCoordinator;
     let registry: Registry;
-
+    let databaseFactory: any;
+    let coordinatorFactory: any;
+    let registryFactory: any;
     let signers: any;
 
+    const testProvider = {
+        publicKey: 123,
+        title: 'testProvider',
+        endpointParams: ['p1', 'p2'],
+        endpoint: 'testEndpoint',
+        query: 'btcPrice',
+        curve: [4, 5, 6],
+        broker: '0x0000000000000000000000000000000000000000'
+    };
+
     beforeEach(async () => {
-        
+
         signers = await ethers.getSigners();
 
-        const coordinatorFactory = await ethers.getContractFactory('ZapCoordinator', signers[0]);
+        coordinatorFactory = await ethers.getContractFactory('ZapCoordinator', signers[0]);
 
-        const registryFactory = await ethers.getContractFactory('Registry', signers[0]);
+        databaseFactory = await ethers.getContractFactory('Database', signers[0]);
+
+        registryFactory = await ethers.getContractFactory('Registry', signers[0]);
+
+        database = (await databaseFactory.deploy()) as Database;
 
         coordinator = (await coordinatorFactory.deploy()) as ZapCoordinator;
         await coordinator.deployed();
 
         registry = (await registryFactory.deploy(coordinator.address)) as Registry;
-        await registry.deployed();
+
+        await database.transferOwnership(coordinator.address);
+        await coordinator.addImmutableContract('DATABASE', database.address);
+        await coordinator.updateContract('REGISTRY', registry.address);
+        await coordinator.updateAllDependencies();
+
+        console.log(await registry.db())
+
     });
 
-    it('Test contracts', () => {
+    it('Should be able to create an instance of the ZapCoordinator contract', () => {
 
-        console.log({
-            registry: registry,
-            coordinator: coordinator
-        })
-    })
+        expect(coordinatorFactory).to.be.ok;
+
+    });
+
+    it('Should be able to create an instance of the Registry contract', () => {
+
+        expect(registryFactory).to.be.ok;
+
+    });
+
+    it('Should be able to deploy the ZapCoordinator contract', () => {
+
+        expect(coordinator).to.be.ok;
+
+    });
+
+    it('Should be able to deploy the Registry contract', () => {
+
+        expect(registry).to.be.ok;
+
+    });
+
+    it('Should initiate a provider', async () => {
+
+        const convertedTitle = ethers.utils.formatBytes32String(testProvider.title);
+
+        console.log(await registry.db())
+    });
 
 })
