eb36d569def30ca65af8a3d0a84d6984f0d6dee7
John <johnm@plasmatech.io>
2021-02-17 15:25:17 -0500

    update dispatch to record query values

--- contracts/lib/platform/OffChainClient.sol
+++ contracts/lib/platform/OffChainClient.sol
@@ -14,28 +14,38 @@ contract OffChainClient is Client1, Client2{
     event Result1(uint256 id, bytes32 response1);
     event Result2(uint256 id, string response1, string response2);
 
+    mapping(uint=>string) public queryResults;
+    mapping(uint=>bytes32[]) public queryBytes32Results;
+
+    mapping(uint=>uint) public queryBytes32IDs;
+    mapping(uint=>uint) public queryIDs;
+    uint public totalQueries;
+    uint public totalBytes32Queries;
     ERC20 token;
     DispatchInterface dispatch;
     BondageInterface bondage;
     RegistryInterface registry;
-    address Oracle;
-    constructor(address tokenAddress, address dispatchAddress, address bondageAddress, address registryAddress,address oracle) public {
+    
+    constructor(address tokenAddress, address dispatchAddress, address bondageAddress, address registryAddress) public {
         token = ERC20(tokenAddress);
         dispatch = DispatchInterface(dispatchAddress);
         bondage = BondageInterface(bondageAddress);
         registry = RegistryInterface(registryAddress);
-        Oracle =oracle;
+        //Oracle =oracle;
     }
     modifier onlyOracle(){
-        require(msg.sender==Oracle);
+        require(msg.sender==address(dispatch),"caller must the dispatch address ");
         _;
     }
     /*
     Implements overloaded callback functions for Client1
     */
     function callback(uint256 id, string calldata response1) external  onlyOracle() {
-        string memory _response1 = response1;
-        emit Result1(id, _response1);
+        //string memory _response1 = response1;
+        queryResults[id]=response1;
+        queryIDs[totalQueries]=id;
+        totalQueries++;
+        emit Result1(id, response1);
         // do something with result
     }
     function Callback(uint256 id, string calldata response1) external  onlyOracle() {
@@ -45,19 +55,48 @@ contract OffChainClient is Client1, Client2{
     }
     function callback(uint256 id, bytes32[]  calldata response) external  onlyOracle() {
         emit Result1(id, response[0]);
+        queryBytes32Results[id]=response;
+        queryBytes32IDs[totalQueries]=id;
+        totalBytes32Queries++;
         // do something with result
     }
 
     // Client2 callback
     function callback(uint256 id, string calldata response1, string calldata response2) external  onlyOracle() {
-        emit Result2(id, response1, response2);
-        // do something with result
+       
+        string memory concat=string(abi.encodePacked(response1,response2));
+        queryResults[id]=concat;
+        queryIDs[totalQueries]=id;
+        totalQueries++;
+         emit Result2(id, response1, response2);
     }
 
     function testQuery(address oracleAddr, string calldata query, bytes32 specifier, bytes32[] calldata params) external returns (uint256) {
         uint256 id = dispatch.query(oracleAddr, query, specifier, params);
         emit MadeQuery(oracleAddr, query, id);
         return id;
+    }
+    function getQueryResultById(uint id) public view returns(string memory ){
+        return queryResults[id];
+    }
+    function getQueryResultByOrder(uint pos) public view returns(string memory){
+        uint id=queryIDs[pos];
+        return queryResults[id];
+    }
+      function getQueryBytes32ResultById(uint id) public view returns(bytes32[] memory){
+        return queryBytes32Results[id];
+    }
+    function getQueryBytes32ResultByOrder(uint pos) public view returns(bytes32[] memory){
+        uint id=queryBytes32IDs[pos];
+        return queryBytes32Results[id];
+    }
+     function getQueryResultByData(uint blockNumber,uint timestamp,string memory query,address subscriber,address provider) public view returns(string memory){
+        uint id=uint256(keccak256(abi.encodePacked(block.number, now, query, subscriber, provider)));
+        return queryResults[id];
+    }
+     function getQueryBytesResultByData(uint blockNumber,uint timestamp,string memory query,address subscriber,address provider) public view returns(bytes32[] memory){
+        uint id=uint256(keccak256(abi.encodePacked(block.number, now,query,  subscriber, provider)));
+        return queryBytes32Results[id];
     }
 
     function stringToBytes32(string memory source) internal pure returns (bytes32 result) {
