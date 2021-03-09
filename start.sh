ce5c1531d9c6a18b1206faf023a39fe35daf48fa
Harris Levine <acyclicgraphs@gmail.com>
2021-03-09 16:02:28 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 npx hardhat node --hostname 0.0.0.0 &
 cat -n /etc/lsb-release
-echo $MODE
-echo $NODE_URL
+# echo $MODE
+# echo $NODE_URL
 sleep 10
 npx hardhat run --network localhost scripts/deploy.ts
 while true; do sleep 1; done
