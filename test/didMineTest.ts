663e00d2fac0a721f7641624224b43e7fa30abcd
Yamada <cool.developer24@gmail.com>
2021-05-27 14:55:59 -0700

    add test and fix issue

---
+++ test/didMineTest.ts
@@ -0,0 +1,162 @@
+import { ethers } from "hardhat";
+
+import { solidity } from "ethereum-waffle";
+
+import chai from "chai";
+
+import { ZapToken } from "../typechain/ZapToken";
+
+import { ZapTransfer } from '../typechain/ZapTransfer';
+
+import { ZapLibrary } from "../typechain/ZapLibrary";
+
+import { ZapDispute } from "../typechain/ZapDispute";
+
+import { ZapStake } from "../typechain/ZapStake";
+
+import { ZapMaster } from "../typechain/ZapMaster";
+
+import { Zap } from "../typechain/Zap";
+import { BigNumber, ContractFactory } from "ethers";
+import { exception } from "console";
+
+const { expect } = chai;
+
+chai.use(solidity);
+
+let zapToken: ZapToken;
+
+let zapTransfer: ZapTransfer;
+
+let zapLibrary: ZapLibrary;
+
+let zapDispute: ZapDispute;
+
+let zapStake: ZapStake;
+
+let zapMaster: ZapMaster;
+
+let zap: Zap;
+
+let signers: any;
+
+var api = "json(https://api.gdax.com/products/BTC-USD/ticker).price";
+
+describe("Did Mine Test", () => {
+
+    beforeEach(async () => {
+
+        signers = await ethers.getSigners();
+
+        const zapTokenFactory: ContractFactory = await ethers.getContractFactory(
+            "ZapToken",
+            signers[0]
+        )
+
+        zapToken = (await zapTokenFactory.deploy()) as ZapToken;
+        await zapToken.deployed()
+
+        const zapTransferFactory: ContractFactory = await ethers.getContractFactory(
+            "ZapTransfer",
+            signers[0]
+        )
+
+        zapTransfer = (await zapTransferFactory.deploy()) as ZapTransfer
+        await zapTransfer.deployed();
+
+        const zapLibraryFactory: ContractFactory = await ethers.getContractFactory("ZapLibrary",
+            {
+                libraries: {
+                    ZapTransfer: zapTransfer.address,
+                },
+                signer: signers[0]
+            }
+        );
+
+        zapLibrary = (await zapLibraryFactory.deploy()) as ZapLibrary
+        await zapLibrary.deployed()
+
+        const zapDisputeFactory: ContractFactory = await ethers.getContractFactory("ZapDispute", {
+
+            libraries: {
+                ZapTransfer: zapTransfer.address,
+            },
+            signer: signers[0]
+
+        });
+
+        zapDispute = (await zapDisputeFactory.deploy()) as ZapDispute
+        await zapDispute.deployed();
+
+        const zapStakeFactory: ContractFactory = await ethers.getContractFactory("ZapStake", {
+
+            libraries: {
+                ZapTransfer: zapTransfer.address,
+                ZapDispute: zapDispute.address
+            },
+            signer: signers[0]
+        })
+
+        zapStake = (await zapStakeFactory.deploy()) as ZapStake
+        await zapStake.deployed()
+
+        const zapFactory: ContractFactory = await ethers.getContractFactory("Zap", {
+
+            libraries: {
+                ZapStake: zapStake.address,
+                ZapDispute: zapDispute.address,
+                ZapLibrary: zapLibrary.address,
+            },
+            signer: signers[0]
+
+        })
+
+        zap = (await zapFactory.deploy(zapToken.address)) as Zap
+        await zap.deployed()
+
+        const zapMasterFactory: ContractFactory = await ethers.getContractFactory("ZapMaster", {
+            libraries: {
+                ZapTransfer: zapTransfer.address,
+                ZapStake: zapStake.address
+            },
+            signer: signers[0]
+        });
+
+        zapMaster = (await zapMasterFactory.deploy(zap.address, zapToken.address)) as ZapMaster
+        await zapMaster.deployed()
+
+    });
+
+    it("Test didMine", async () => {
+
+        for(var i = 1;i<=5;i++){
+            await zapToken.allocate(signers[i].address, 2000);
+        }
+
+        for(var i = 1;i<=5;i++) {
+
+            // Attach the ZapMaster instance to Zap
+            zap = zap.attach(zapMaster.address);
+
+            // Connects address 1 as the signer
+            zap = zap.connect(signers[i]);
+
+            // Stakes 1000 Zap to initiate a miner
+            await zap.depositStake();
+        }
+
+        console.log(zap);
+
+        // zap = zap.attach(zapMaster.address);
+
+        //for(var i=0; i<52;i++) {
+            
+            const x = "USD";
+            const apix = api;
+            await zap.requestData(apix, x, 1000, 52);
+        //}
+
+        // const getReqQ = await zapMaster.getRequestQ();
+        // console.log("getReqQ ============", getReqQ);
+    })
+});
