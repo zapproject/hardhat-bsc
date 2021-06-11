6d2289a57b64205a570259ce249ccdbe4b91e6bb
Kimani Kelly <kimanikelly@yahoo.com>
2021-06-11 14:49:52 -0400

    Revert "Feat/dynamic mining solutions"

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,10 +2,6 @@ import { config as dotEnvConfig } from "dotenv";
 dotEnvConfig();
 
 import { HardhatUserConfig } from "hardhat/types";
-require("hardhat-gas-reporter")
-
-import "hardhat-gas-reporter"
-
 
 import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-etherscan";
@@ -20,13 +16,7 @@ import './tasks/initProviderCurve';
 import './tasks/setEndpointParams';
 import './tasks/bond';
 import './tasks/dispatch';
-import './tasks/dispatchCoinGecko';
-import './tasks/dispatchCGPriceClient';
-import './tasks/dispatchBittrex';
-import './tasks/checkClient';
 
-import {getBSCGasPrice} from './scripts/getGasPrice'
-const fs = require('fs');  // required for reading BSC gas price
 
 // TODO: reenable solidity-coverage when it works
 // import "solidity-coverage";
@@ -36,63 +26,33 @@ const RINKEBY_PRIVATE_KEY =
   process.env.RINKEBY_PRIVATE_KEY! ||
   "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3"; // well known private key
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
-const KOVAN_PRIVATE_KEY = process.env.KOVAN_PRIVATE_KEY ||
-  "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3";
 
-getBSCGasPrice()
+const config: HardhatUserConfig = {
 
-const config = {
   solidity: {
-    compilers: [
-      { version: '0.4.24', settings: {} },
-      { version: '0.5.1', settings: {} }
-    ]
-  },
-  gasReporter: {
-    enabled: true,
-    currency: 'USD',
-    gasPrice: 0.0,
-    coinmarketcap: process.env.COINMARKETCAP_API_KEY
+    compilers: [{ version: "0.4.24", settings: {} }, { version: "0.5.1", settings: {} }],
   },
   networks: {
     localhost: {
-      url: 'http://127.0.0.1:8545/'
+      url: "http://127.0.0.1:8545/",
+
     },
     hardhat: {
-      gasPrice: 8000000000
+
     },
     rinkeby: {
       url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [RINKEBY_PRIVATE_KEY]
-    },
-    kovan: {
-      url: `https://kovan.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [KOVAN_PRIVATE_KEY]
+      accounts: [RINKEBY_PRIVATE_KEY],
     },
     coverage: {
-      url: 'http://127.0.0.1:8555' // Coverage launches its own ganache-cli client
-    }
+      url: "http://127.0.0.1:8555", // Coverage launches its own ganache-cli client
+    },
   },
   etherscan: {
     // Your API key for Etherscan
     // Obtain one at https://etherscan.io/
-    apiKey: ETHERSCAN_API_KEY
+    apiKey: ETHERSCAN_API_KEY,
   },
-  mocha: {
-    timeout: 1000000
-  }
 };
-
-// read BSC gas price and assign the gas reporter and hardhat network's gas price to it
-try {
-  let data = fs.readFileSync("./output/bscGas.txt", 'utf8')
-  data = data.replaceAll('"', '')
-  data = Number.parseInt(data)
-  config.networks.hardhat.gasPrice = data
-  let parsedData = (data / 1000000000).toFixed(2)  // round to 2 decimal places
-  config.gasReporter.gasPrice = Number(parsedData)
-} catch (err) {
-  console.error(err)
-}
 
 export default config;
