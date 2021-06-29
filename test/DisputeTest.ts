84edb825c2b61d0e54d9285188b91d79feff8c55
Jayon Kim <jpkim921@gmail.com>
2021-06-29 18:08:34 -0400

    start disputeTest

--- test/DisputeTest.ts
+++ test/DisputeTest.ts
@@ -1,3 +1,5 @@
+// Signers 15, 16, 17, 18, 19, 0 are already miners
+
 import { ethers } from "hardhat";
 
 import { solidity } from "ethereum-waffle";
@@ -17,6 +19,7 @@ import { ZapStake } from "../typechain/ZapStake";
 import { ZapMaster } from "../typechain/ZapMaster";
 
 import { Zap } from "../typechain/Zap";
+
 import { BigNumber, ContractFactory } from "ethers";
 
 const { expect } = chai;
@@ -39,14 +42,14 @@ let zap: Zap;
 
 let signers: any;
 
-describe("Did Mine Test", () => {
+describe("Test ZapDispute and it's dispute functions", () => {
 
     beforeEach(async () => {
 
         signers = await ethers.getSigners();
 
         const zapTokenFactory: ContractFactory = await ethers.getContractFactory(
-            "ZapToken",
+            "ZapTokenBSC",
             signers[0]
         )
 
@@ -122,99 +125,84 @@ describe("Did Mine Test", () => {
         zapMaster = (await zapMasterFactory.deploy(zap.address, zapTokenBsc.address)) as ZapMaster
         await zapMaster.deployed()
 
-        for (var i = 1; i <= 5; i++) {
-
-            // Allocates ZAP to signers 1 - 5
+        // stake signers 1 to 5.
+        for (let i=1; i<=5; i++) {
             await zapTokenBsc.allocate(signers[i].address, 1300);
-
-        }
-
-    });
-
-    it("Test didMine", async () => {
-
-        let x: string;
-
-        let apix: string;
-
-        // Request string
-        const api: string = "json(https://api.pro.coinbase.com/products/ETH-USD/ticker).price";
-
-        // Allocates 5000 ZAP to signer 0 
-        // await zapTokenBsc.allocate(signers[0].address, 5000);
-        await zapTokenBsc.allocate(zapMaster.address, 5000);
-
-        // Iterates through signers 1 through 5
-        for (var i = 1; i <= 5; i++) {
-
-            // Attach the ZapMaster instance to Zap
             zap = zap.attach(zapMaster.address);
-
-            // Connects addresses 1-5 as the signer
             zap = zap.connect(signers[i]);
-
-            // Stakes 1000 Zap to initiate a miner
             await zap.depositStake();
         }
 
+        
+        let symbol: string = "BTC/USD"
+        // Request string
+        const api: string = "json(https://api.binance.com/api/v1/klines?symbol=BTCUSDT&interval=1d&limit=1).0.4";
+        
+        await zapTokenBsc.approve(zap.address, 5000);
         zap = zap.connect(signers[0]);
-
-        // Approves Zap.sol the amount to tip for requestData
-        await zapTokenBsc.approve(zap.address, 5000)
-
-        // Iterates the length of the requestQ array
-        for (var i = 0; i < 52; i++) {
-
-            x = "USD" + i;
-            apix = api + i;
-
-            // Submits the query string as the request
-            // Each request will add a tip starting at 51 and count down until 0
-            // Each tip will be stored inside the requestQ array
-            await zap.requestData(apix, x, 1000, 52 - i);
-        }
-
-        // Gets the tip amounts stored in the requestQ array
-        // Values are returned as hexStrings
-        const getReqQ: BigNumber[] = await zapMaster.getRequestQ();
-
-        // Parses the getReqQ array from hexStrings to numbers
-        const reqQ: number[] = getReqQ.map(item => parseInt(item._hex));
-
-        for (var i = 1; i <= 5; i++) {
-
-            // Connects address 1 as the signer
-            zap = zap.connect(signers[i]);
-
-            /*
-            Gets the data properties for the current request
-            bytes32 _challenge,
-            uint256[5] memory _requestIds,
-            uint256 _difficutly,
-            uint256 _tip
-            */
-            const newCurrentVars: any = await zap.getNewCurrentVariables();
-
-            // Each Miner will submit a mining solution
-            await zap.submitMiningSolution("nonce", 1, 1200);
-
-            // Checks if the miners mined the challenge
-            // true = Miner did mine the challenge
-            // false = Miner did not mine the challenge
-            const didMineStatus: boolean = await zapMaster.didMine(
-                newCurrentVars[0],
-                signers[i].address
-            );
-
-            // Expects the challenge hash 66 characters in length
-            expect(newCurrentVars[0]).to.have.length(66);
-
-            // Expects the didMine status of each miner to be true
-            expect(didMineStatus).to.be.true;
-
-        }
-
-        expect(reqQ).to.have.length(51);
+        await zap.requestData(api, symbol, 1000, 52);
 
     })
-});
+
+    it("Should stake a miner with a balance greater than or equal to 1000 ZAP and return a 1 stake status and an above 0 timestamp",
+        async () => {
+          // expect(stakerInfo[1]).to.greaterThan(0)
+          let bal = await zapTokenBsc.balanceOf(signers[1].address);
+          console.log(bal);
+          expect(bal).to.equal(1300);
+
+
+          const getInfo: BigNumber[] = await zapMaster.getStakerInfo(
+            signers[5].address
+          );
+          const stakerInfo: number[] = getInfo.map(info => parseInt(info._hex));
+          expect(stakerInfo[0]).to.equal(1), 'Staked';
+          
+
+          // // stakerInfo[0] = Staker Status
+          // // Expect the staker status to be 1
+          // expect(stakerInfo[0]).to.equal(1), "Staked";
+
+          // // stakerInfo[1] = Staker Timestamp
+          // // Expect the staker timestamp to be greater than 0
+          // expect(stakerInfo[1]).to.greaterThan(0)
+
+          // // Allocate enough to stake
+          // await zapTokenBsc.allocate(signers[1].address, 1000)
+
+          // // Attach the ZapMaster instance to Zap
+          // zap = zap.attach(zapMaster.address);
+
+          // // Connects address 1 as the signer
+          // zap = zap.connect(signers[1]);
+
+          // // Stakes 1000 Zap to initiate a miner
+          // await zap.depositStake();
+
+          // // Gets the balance as hexString
+          // const getBalance: BigNumber = await zapMaster.balanceOf(signers[1].address);
+
+          // // Parses the hexString
+          // const balance: number = parseInt(getBalance._hex);
+
+          // // Returns an array containing the staker status and timestamp
+          // // The array values are returned as hexStrings
+          // const getInfo: BigNumber[] = await zapMaster.getStakerInfo(signers[1].address);
+
+          // // Parses the hexStrings in the array
+          // const stakerInfo: number[] = getInfo.map(info => parseInt(info._hex));
+
+          // // Expect the balance to be greater than or equal to 1000
+          // expect(balance).to.be.greaterThanOrEqual(1000);
+
+          //   stakerInfo[0] = Staker Status
+          // // Expect the staker status to be 1
+          // expect(stakerInfo[0]).to.equal(1), "Staked";
+
+          // // stakerInfo[1] = Staker Timestamp
+          // // Expect the staker timestamp to be greater than 0
+          // expect(stakerInfo[1]).to.greaterThan(0)
+        }
+    )
+
+})
