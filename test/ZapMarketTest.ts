a390460805f282f7162009b0319bb8524f12ab2f
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2021-08-19 14:37:15 -0400

    ZapMarket Tests Should reject if the bid shares havent been set yet test passing

--- test/ZapMarketTest.ts
+++ test/ZapMarketTest.ts
@@ -415,9 +415,21 @@ describe("ZapMarket Test", () => {
 
         it("Should reject if the bid shares haven't been set yet", async () => {
 
-            await expect(zapMarket.connect(signers[1]).setAsk(1, ask1)).to.be.revertedWith(
+            await expect(zapMarket.connect(signers[1]).setAsk(
+                zapMedia1.address,
+                1,
+                ask1
+            )).to.be.revertedWith(
                 'Market: Invalid bid shares for token'
-            )
+            );
+
+            await expect(zapMarket.connect(signers[2]).setAsk(
+                zapMedia2.address,
+                1,
+                ask1
+            )).to.be.revertedWith(
+                'Market: Invalid bid shares for token'
+            );
 
         });
 
