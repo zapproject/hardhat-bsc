25d69916cb71a2b6b4e9a0ca65f8a0a4e85c4bb3
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2021-08-30 12:57:05 -0400

    Imported the upgrades library into the ZapMarket deploy script

--- deploy/01_deploy_zap_market.ts
+++ deploy/01_deploy_zap_market.ts
@@ -1,6 +1,6 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
-
 import { DeployFunction } from 'hardhat-deploy/types'
+import { upgrades } from 'hardhat';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const { deployments, getNamedAccounts } = hre
