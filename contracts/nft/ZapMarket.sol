49db01e74bd50a35d67915ca7d7c38439d863e33
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2021-08-31 16:03:36 -0400

    Import IERC20Upgradeable into ZapMarket

--- contracts/nft/ZapMarket.sol
+++ contracts/nft/ZapMarket.sol
@@ -6,11 +6,13 @@ pragma experimental ABIEncoderV2;
 import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {IERC20Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {Decimal} from "./Decimal.sol";
 import {ZapMedia} from "./ZapMedia.sol";
 import {IMarket} from "./interfaces/IMarket.sol";
 import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+
 import "hardhat/console.sol";
 
 /**
