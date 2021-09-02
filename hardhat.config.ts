74011aa3e58db4d8cd24fe4aa5554c736bd53ae4
Kimani Kelly <kimanikelly@Kimanis-MBP.fios-router.home>
2021-09-02 14:34:07 -0400

    Uncomment the testnet mnemonic

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -72,7 +72,7 @@ const config = {
       url: "https://data-seed-prebsc-1-s1.binance.org:8545",
       chainId: 97,
       gasPrice: 20000000000,
-      // accounts: { mnemonic: process.env.MNEMONIC }
+      accounts: { mnemonic: process.env.MNEMONIC }
     },
     hardhat: {
       gasPrice: 8000000000,
