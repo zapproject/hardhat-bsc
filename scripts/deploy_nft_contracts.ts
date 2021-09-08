373302df2e3fd3f2f9b54df1f1bc61c02f2086ed
kimani kelly <kimanikelly@yahoo.com>
2021-09-08 12:46:17 -0400

    Bugfixes ZapMarket & ZapMedia tests are passing after merge The NFT deploy script deploys is able to deploy both the upgradeable Market & Media contracts

--- scripts/deploy_nft_contracts.ts
+++ scripts/deploy_nft_contracts.ts
@@ -7,40 +7,23 @@ async function main() {
 
     const tokenAddress = (await hre.deployments.get('ZapTokenBSC')).address;
 
-    const ZapMarketImplementation = await ethers.getContractFactory('ZapMarket', signers[0]);
-    const zapMarketImplementation = await ZapMarketImplementation.deploy();
-    await zapMarketImplementation.deployed();
-    console.log('ZapMarket Implementation deployed to:', zapMarketImplementation.address);
-
     const ZapMarketProxy = await ethers.getContractFactory('ZapMarket', signers[0]);
     const zapMarketProxy = await upgrades.deployProxy(ZapMarketProxy, { initializer: 'initialize' });
     await zapMarketProxy.deployed();
     console.log('ZapMarket Proxy deployed to:', zapMarketProxy.address);
 
+    const ZapMediaProxy = await ethers.getContractFactory('ZapMedia', signers[0]);
+    const zapMediaProxy = await upgrades.deployProxy(
+        ZapMediaProxy,
+        ["ZapMedia", "ZAPBSC", zapMarketProxy.address, true],
+    );
+    await zapMediaProxy.deployed();
+    console.log('ZapMedia Proxy deployed to:', zapMediaProxy.address);
 
-    const ZapMedia = await ethers.getContractFactory('ZapMedia', signers[0])
-
-    const zapMedia = await ZapMedia.deploy()
-
-    await zapMedia.deployed()
-
-    console.log(zapMedia.address)
-    console.log(await zapMedia.symbol())
-
-    // const test = await ethers.getContractFactory('ZapMarketV2', signers[0])
-
-    // const x = await upgrades.upgradeProxy(zapMarketProxy.address, test)
-
-
-    // console.log(await x.getConfigStatus(zapMediaImplementation.address));
-
-    // await zapMediaImplementation.deployed();
-    // console.log('ZapMedia Implementation deployed to:', zapMediaImplementation.address);
-
-    // // AuctionHouse may be changed in the future to be upgradeable
-    // // AucionHouse will change to handle more than one Media contract
+    // AuctionHouse may be changed in the future to be upgradeable
+    // AucionHouse will change to handle more than one Media contract
     // const AuctionHouse = await ethers.getContractFactory('AuctionHouse', signers[0]);
-    // const auctionHouse = await AuctionHouse.deploy(zapMedia.address, tokenAddress);
+    // const auctionHouse = await AuctionHouse.deploy(zapMediaProxy.address, tokenAddress);
     // await auctionHouse.deployed();
     // console.log('AuctionHouse deployed to:', auctionHouse.address);
 
