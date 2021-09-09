78cc2c2d206c46c8e0d434ea972888fa2148079f
kimani kelly <kimanikelly@yahoo.com>
2021-09-09 08:13:46 -0400

    Commit to pull from develop

--- test/utils.ts
+++ test/utils.ts
@@ -87,3 +87,92 @@ export const approveAuction = async (
 
 export const revert = (messages: TemplateStringsArray) =>
   `VM Exception while processing transaction: revert ${messages[0]}`;
+// @ts-ignore
+import { ethers } from "hardhat";
+import {
+  MarketFactory,
+  Media,
+  MediaFactory,
+} from "@zoralabs/core/dist/typechain";
+import {
+  BadBidder,
+  AuctionHouse,
+  WETH,
+  BadERC721,
+  TestERC721,
+} from "../typechain";
+import { sha256 } from "ethers/lib/utils";
+import Decimal from "../utils/Decimal";
+import { BigNumber } from "ethers";
+
+export const THOUSANDTH_ETH = ethers.utils.parseUnits(
+  "0.001",
+  "ether"
+) as BigNumber;
+export const TENTH_ETH = ethers.utils.parseUnits("0.1", "ether") as BigNumber;
+export const ONE_ETH = ethers.utils.parseUnits("1", "ether") as BigNumber;
+export const TWO_ETH = ethers.utils.parseUnits("2", "ether") as BigNumber;
+
+export const deployWETH = async () => {
+  const [deployer] = await ethers.getSigners();
+  return (await (await ethers.getContractFactory("WETH")).deploy()) as WETH;
+};
+
+export const deployOtherNFTs = async () => {
+  const bad = (await (
+    await ethers.getContractFactory("BadERC721")
+  ).deploy()) as BadERC721;
+  const test = (await (
+    await ethers.getContractFactory("TestERC721")
+  ).deploy()) as TestERC721;
+
+  return { bad, test };
+};
+
+export const deployZoraProtocol = async () => {
+  const [deployer] = await ethers.getSigners();
+  const market = await (await new MarketFactory(deployer).deploy()).deployed();
+  const media = await (
+    await new MediaFactory(deployer).deploy(market.address)
+  ).deployed();
+  await market.configure(media.address);
+  return { market, media };
+};
+
+export const deployBidder = async (auction: string, nftContract: string) => {
+  return (await (
+    await (await ethers.getContractFactory("BadBidder")).deploy(
+      auction,
+      nftContract
+    )
+  ).deployed()) as BadBidder;
+};
+
+export const mint = async (media: Media) => {
+  const metadataHex = ethers.utils.formatBytes32String("{}");
+  const metadataHash = await sha256(metadataHex);
+  const hash = ethers.utils.arrayify(metadataHash);
+  await media.mint(
+    {
+      tokenURI: "zora.co",
+      metadataURI: "zora.co",
+      contentHash: hash,
+      metadataHash: hash,
+    },
+    {
+      prevOwner: Decimal.new(0),
+      owner: Decimal.new(85),
+      creator: Decimal.new(15),
+    }
+  );
+};
+
+export const approveAuction = async (
+  media: Media,
+  auctionHouse: AuctionHouse
+) => {
+  await media.approve(auctionHouse.address, 0);
+};
+
+export const revert = (messages: TemplateStringsArray) =>
+  `VM Exception while processing transaction: revert ${messages[0]}`;
