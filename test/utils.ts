a4b764faae21ca46e9e4be5e7400de56d91a97c4
Jamil Bousquet <jamil.bousquet@gmail.com>
2021-09-09 09:41:00 -0400

    Fixed utils for variable medias on AuctionHouse

--- test/utils.ts
+++ test/utils.ts
@@ -1,5 +1,111 @@
+// @ts-ignore
+import { ethers, upgrades } from "hardhat";
+import {
+  ZapMarket, ZapMedia
+} from "../typechain"
+import {
+  BadBidder,
+  AuctionHouse,
+  WETH,
+  BadERC721,
+  TestERC721,
+} from "../typechain";
+import { sha256 } from "ethers/lib/utils";
+import Decimal from "../utils/Decimal";
+import { BigNumber } from "ethers";
 import { fromRpcSig } from 'ethereumjs-util';
-import { ZapMedia } from '../typechain/ZapMedia';
+
+
+export const THOUSANDTH_ETH = ethers.utils.parseUnits(
+  "0.001",
+  "ether"
+) as BigNumber;
+export const TENTH_ETH = ethers.utils.parseUnits("0.1", "ether") as BigNumber;
+export const ONE_ETH = ethers.utils.parseUnits("1", "ether") as BigNumber;
+export const TWO_ETH = ethers.utils.parseUnits("2", "ether") as BigNumber;
+
+export const deployWETH = async () => {
+  const [deployer] = await ethers.getSigners();
+  return (await (await ethers.getContractFactory("WETH")).deploy()) as WETH;
+};
+
+export const deployOtherNFTs = async () => {
+  const bad = (await (
+    await ethers.getContractFactory("BadERC721")
+  ).deploy()) as BadERC721;
+  const test = (await (
+    await ethers.getContractFactory("TestERC721")
+  ).deploy()) as TestERC721;
+
+  return { bad, test };
+};
+
+export const deployZapNFTMarketplace = async () => {
+  let market: ZapMarket
+  let media1: ZapMedia
+  let media2: ZapMedia
+  let media3: ZapMedia
+
+  const [deployer0, deployer1, deployer2, deployer3] = await ethers.getSigners();
+  const marketFactory = await ethers.getContractFactory("ZapMarket", deployer0);
+  market = await upgrades.deployProxy(marketFactory, { initializer: "initialize" }) as ZapMarket;
+
+  const mediaFactory1 = await ethers.getContractFactory("ZapMedia", deployer1);
+  media1 = await (
+    await upgrades.deployProxy(mediaFactory1, ["Test Media 1", "TM1", market.address])
+  ).deployed() as ZapMedia;
+
+  const mediaFactory2 = await ethers.getContractFactory("ZapMedia", deployer2);
+  media2 = await (
+    await upgrades.deployProxy(mediaFactory2, ["Test Media 1", "TM1", market.address])
+  ).deployed() as ZapMedia;
+
+  const mediaFactory3 = await ethers.getContractFactory("ZapMedia", deployer3);
+  media3 = await (
+    await upgrades.deployProxy(mediaFactory3, ["Test Media 1", "TM1", market.address])
+  ).deployed() as ZapMedia;
+
+
+  return { market, media1, media2, media3 };
+};
+
+export const deployBidder = async (auction: string, nftContract: string) => {
+  return (await (
+    await (await ethers.getContractFactory("BadBidder")).deploy(
+      auction,
+      nftContract
+    )
+  ).deployed()) as BadBidder;
+};
+
+export const mint = async (media: ZapMedia) => {
+  const metadataHex = ethers.utils.formatBytes32String("{}");
+  const metadataHash = await sha256(metadataHex);
+  const hash = ethers.utils.arrayify(metadataHash);
+  await media.mint(
+    {
+      tokenURI: "zora.co",
+      metadataURI: "zora.co",
+      contentHash: hash,
+      metadataHash: hash,
+    },
+    {
+      prevOwner: Decimal.new(0),
+      owner: Decimal.new(85),
+      creator: Decimal.new(15),
+    }
+  );
+};
+
+export const approveAuction = async (
+  media: ZapMedia,
+  auctionHouse: AuctionHouse
+) => {
+  await media.approve(auctionHouse.address, 0);
+};
+
+export const revert = (messages: TemplateStringsArray) =>
+  `VM Exception while processing transaction: revert ${messages[0]}`;
 
 export async function signPermit (
 	zapMedia1: ZapMedia,
