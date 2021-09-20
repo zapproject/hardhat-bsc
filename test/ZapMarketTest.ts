3f84052fd86cde59f8eaeb3e9ec90e29ab2f9088
kimani kelly <kimanikelly@yahoo.com>
2021-09-20 13:30:23 -0400

    BidFinalized event is able to emit both media contracts that accepted bids

--- test/ZapMarketTest.ts
+++ test/ZapMarketTest.ts
@@ -1505,33 +1505,17 @@ describe('ZapMarket Test', () => {
       const marketPostBal = await zapTokenBsc.balanceOf(zapMarket.address);
       expect(parseInt(marketPostBal._hex)).to.equal(bid1.amount + bid2.amount);
 
-
-
-
-
-
-
       await zapMedia1.acceptBid(0, bid1);
       await zapMedia2.acceptBid(0, bid2);
-
-
 
       const zapMarketFilter: EventFilter =
         zapMarket.filters.BidFinalized(null, null, null);
 
-      const event: Event = (await zapMarket.queryFilter(zapMarketFilter))[0];
-
-      console.log(event)
+      const event = (await zapMarket.queryFilter(zapMarketFilter))
 
 
-
-
-
-
-
-
-
-
+      expect(zapMedia1.address).to.equal(event[0].args[2]);
+      expect(zapMedia2.address).to.equal(event[1].args[2]);
 
       const marketBal = await zapTokenBsc.balanceOf(zapMarket.address);
       expect(parseInt(marketBal._hex)).to.equal(0);
