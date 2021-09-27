3fd509417f2ebb3c6bcfed165284543c970801a8
kimani kelly <kimanikelly@yahoo.com>
2021-09-27 15:41:41 -0400

    Add internal specifier to the tokens instance

--- contracts/nft/MediaGetter.sol
+++ contracts/nft/MediaGetter.sol
@@ -3,31 +3,46 @@ pragma solidity ^0.8.4;
 
 import {MediaGettersLib} from './libraries/MediaGettersLib.sol';
 import {MediaStorage} from './libraries/MediaStorage.sol';
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
 
 /// @title A title that should describe the contract/interface
 /// @author The name of the author
 /// @notice Explain to an end user what this does
 /// @dev Explain to a developer any extra details
-contract MediaGetter is Initializable{
+contract MediaGetter is Initializable {
     using MediaGettersLib for MediaStorage.Tokens;
 
-    MediaStorage.Tokens tokens;
+    MediaStorage.Tokens internal tokens;
 
-    function getTokenCreators(uint256 _tokenId) public view returns (address creator) {
+    function getTokenCreators(uint256 _tokenId)
+        public
+        view
+        returns (address creator)
+    {
         creator = tokens.getTokenCreators(_tokenId);
     }
 
-    function getPreviousTokenOwners(uint256 _tokenId) public view returns (address prevOwner) {
+    function getPreviousTokenOwners(uint256 _tokenId)
+        public
+        view
+        returns (address prevOwner)
+    {
         prevOwner = tokens.getPreviousTokenOwners(_tokenId);
     }
 
-    function getTokenContentHashes(uint256 _tokenId) public view returns (bytes32 contentHash) {
+    function getTokenContentHashes(uint256 _tokenId)
+        public
+        view
+        returns (bytes32 contentHash)
+    {
         contentHash = tokens.getTokenContentHashes(_tokenId);
     }
 
-    function getTokenMetadataHashes(uint256 _tokenId) public view returns (bytes32 metadataHash) {
+    function getTokenMetadataHashes(uint256 _tokenId)
+        public
+        view
+        returns (bytes32 metadataHash)
+    {
         metadataHash = tokens.getTokenMetadataHashes(_tokenId);
     }
-
 }
