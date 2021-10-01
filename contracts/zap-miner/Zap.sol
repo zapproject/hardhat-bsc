d15f66d1c01da99d621712c3e7c96122ac2b62c6
Jamil Bousquet <jamil.bousquet@gmail.com>
2021-10-01 13:07:00 -0400

    removed redundant `_callOptionalReturn`

--- contracts/zap-miner/Zap.sol
+++ contracts/zap-miner/Zap.sol
@@ -603,30 +603,4 @@ contract Zap {
             require(abi.decode(returndata, (bool)), "ZapTokenBSC: ERC20 operation did not succeed");
         }
     }
-
-    /**
-     * @dev Imitates a Solidity high-level call (i.e. a regular function call to a contract), relaxing the requirement
-     * on the return value: the return value is optional (but if data is returned, it must not be false).
-     * @param _token The token targeted by the call.
-     * @param data The call data (encoded using abi.encode or one of its variants).
-     */
-    function _callOptionalReturn(ZapTokenBSC _token, bytes memory data)
-        private
-    {
-        // We need to perform a low level call here, to bypass Solidity's return data size checking mechanism, since
-        // we're implementing it ourselves. We use {Address.functionCall} to perform this call, which verifies that
-        // the target address contains contract code and also asserts for success in the low-level call.
-
-        bytes memory returndata = address(_token).functionCall(
-            data,
-            'ZapTokenBSC: low-level call failed'
-        );
-        if (returndata.length > 0) {
-            // Return data is optional
-            require(
-                abi.decode(returndata, (bool)),
-                'ZapTokenBSC: ERC20 operation did not succeed'
-            );
-        }
-    }
 }
