f0cd8bfd2d750b7e40f1b6442b64c5abb0437fb5
Jamil <jamil.bousquet@gmail.com>
2021-10-13 13:08:46 -0400

    removed bool return of setTokenDetails; validating media contract address from within this function instead

--- contracts/nft/AuctionHouse.sol
+++ contracts/nft/AuctionHouse.sol
@@ -72,21 +72,22 @@ contract AuctionHouse is IAuctionHouse, ReentrancyGuardUpgradeable {
 
     function setTokenDetails(uint256 tokenId, address mediaContract)
         internal
-        returns (bool)
     {
         require(
             mediaContract != address(0),
             'AuctionHouse: Media Contract Address can not be the zero address'
         );
-        if (tokenDetails[mediaContract][tokenId].mediaContract != address(0))
-            return false;
+
+        if (tokenDetails[mediaContract][tokenId].mediaContract != address(0)){
+            require(
+                mediaContract == tokenDetails[mediaContract][tokenId].mediaContract,
+                "Token is already set for a different collection");
+        }
 
         tokenDetails[mediaContract][tokenId] = TokenDetails({
             tokenId: tokenId,
             mediaContract: mediaContract
         });
-
-        return true;
     }
 
     /**
@@ -127,11 +128,7 @@ contract AuctionHouse is IAuctionHouse, ReentrancyGuardUpgradeable {
         uint256 auctionId = _auctionIdTracker.current();
         _auctionIdTracker.increment();
 
-        if (setTokenDetails(tokenId, mediaContract) == false) {
-            require(
-                mediaContract == tokenDetails[mediaContract][tokenId].mediaContract,
-                "Token is already set for a different collection");
-        }
+        setTokenDetails(tokenId, mediaContract);
 
         auctions[auctionId] = Auction({
             token: tokenDetails[mediaContract][tokenId],
