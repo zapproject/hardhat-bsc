cada3c3eec75ac09231c458c05dd313b70315fdb
minciong <maxraphael_inciong@yahoo.com>
2021-10-28 15:31:00 -0400

    removed commennt

--- test/ZapMarketTest.ts
+++ test/ZapMarketTest.ts
@@ -1608,8 +1608,8 @@ describe('ZapMarket Test', () => {
     it('Testing', async () => {
       console.log(creatureFactory.signer.getAddress());
       console.log('zero',signers[0].address)
-      await creatureFactory.connect(signers[1]).mint(0, owner.address);
-      // expect()
+      await creatureFactory.connect(signers[1]).mint(0, signers[0].address);
+      
     })
 
 
