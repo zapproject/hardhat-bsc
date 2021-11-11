e3c4da6bebeb3833a5cf43cc94a7d8e0d530883a
Jamil Bousquet <jamil.bousquet@gmail.com>
2021-11-11 17:35:47 -0400

    updated to latest contract changes

--- scripts/deploy_nft_contracts.ts
+++ scripts/deploy_nft_contracts.ts
@@ -1,5 +1,14 @@
-import { ethers, upgrades } from "hardhat";
-import { BigNumber } from 'ethers';
+import { ethers, upgrades, run } from "hardhat";
+import {
+    AuctionHouse,
+    MediaFactory,
+    ZapMarket,
+    ZapMedia,
+    ZapVault
+} from '../typechain/'
+import { BigNumber, EventFilter, Event } from 'ethers';
+
+const ZMABI = require('../artifacts/contracts/nft/ZapMedia.sol/ZapMedia.json').abi;
 
 async function main() {
 
@@ -19,18 +28,18 @@ async function main() {
         ZapVault,
         [tokenAddress],
         { initializer: 'initializeVault' }
-    );
+    ) as ZapVault;
     await zapVault.deployed();
-    console.log("ZapVault deployed to:", zapVault.address);
+    console.log("ZapVault deployed to: ", zapVault.address);
 
     const ZapMarket = await ethers.getContractFactory('ZapMarket', signers[0]);
     const zapMarket = await upgrades.deployProxy(
         ZapMarket,
         [zapVault.address],
         { initializer: 'initializeMarket' }
-    );
+    ) as ZapMarket;
     await zapMarket.deployed();
-    console.log('ZapMarket deployed to:', zapMarket.address);
+    console.log('ZapMarket deployed to: ', zapMarket.address);
 
     await zapMarket.setFee(platformFee);
 
@@ -39,35 +48,51 @@ async function main() {
         MediaFactory,
         [zapMarket.address],
         { initializer: 'initialize' }
-    );
+    ) as MediaFactory;
     await mediaFactory.deployed();
-    console.log('MediaFactory deployed to:', zapMarket.address);
+    console.log('MediaFactory deployed to: ', zapMarket.address);
 
     await zapMarket.setMediaFactory(mediaFactory.address);
     console.log("MediaFactory set to ZapMarket");
 
-    const ZapMedia = await ethers.getContractFactory('ZapMedia', signers[0]);
-    const zapMedia = await upgrades.deployProxy(
-        ZapMedia,
-        [
-            "ZapMedia",
-            "ZAPBSC",
-            zapMarket.address,
-            true,
-            'https://ipfs.moralis.io:2053/ipfs/Qmb6X5bYB3J6jq9JPmd5FLx4fa4JviXfV11yN42i96Q5Xt'
-        ],
-        { initializer: 'initialize' }
-    );
+    const data = {
+        name: "ZapMedia",
+        symbol: "ZAPBSC",
+        marketContractAddr: zapMarket.address,
+        permissive: true,
+        _collectionMetadata:'https://ipfs.moralis.io:2053/ipfs/Qmb6X5bYB3J6jq9JPmd5FLx4fa4JviXfV11yN42i96Q5Xt'
+    }
+
+    const txReceipt = await mediaFactory.deployMedia(
+        data.name, data.symbol, data.marketContractAddr, data.permissive, data._collectionMetadata
+        );
+
+    const mediaDeployedEventFilter: EventFilter = mediaFactory.filters.MediaDeployed(null);
+    const mediaDeployedEvent: Event = (await mediaFactory.queryFilter(mediaDeployedEventFilter))[0];
+    const zapMediaAddr = mediaDeployedEvent.args?.mediaContract;
+    
+    const zapMedia = new ethers.Contract(zapMediaAddr, ZMABI, signers[0]) as ZapMedia;
     await zapMedia.deployed();
-    console.log('ZapMedia deployed to:', zapMedia.address);
+    console.log('ZapMedia deployed to: ', zapMedia.address);
+    
+    // console.log('Verifing ZapMedia...')
+    // await run("verify:verify", {
+    //     address: zapMedia.address,
+    //     constructorArguments: [
+    //         data.name, data.symbol, data.marketContractAddr, data.permissive, data._collectionMetadata
+    //     ]
+    // });
 
     const AuctionHouse = await ethers.getContractFactory('AuctionHouse', signers[0]);
     const auctionHouse = await upgrades.deployProxy(AuctionHouse,
         [tokenAddress, zapMarket.address],
         { initializer: 'initialize' }
-    );
+    ) as AuctionHouse;
     await auctionHouse.deployed();
-    console.log('AuctionHouse deployed to:', auctionHouse.address);
+    console.log('AuctionHouse deployed to: ', auctionHouse.address);
+
+    await zapMarket.setAuctionHouse(auctionHouse.address);
+    console.log('AuctionHouse is set on ZapMarket');
 
 }
 
