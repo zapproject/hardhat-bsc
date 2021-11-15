760f01e8218a81c5ab2b4d1db0fecd7da8c305a9
Jamil Bousquet <jamil.bousquet@gmail.com>
2021-11-15 18:23:14 -0400

    added "Old"/V1 contracts from develop

---
+++ contracts/nft/develop/MediaFactoryOld.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.4;
+pragma experimental ABIEncoderV2;
+
+import {OwnableUpgradeable} from '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
+
+import {ZapMediaOld} from './ZapMediaOld.sol';
+import {ZapMarketOld} from './ZapMarketOld.sol';
+
+contract MediaFactoryOld is OwnableUpgradeable {
+    event MediaDeployed(address indexed mediaContract);
+
+    ZapMarketOld zapMarket;
+
+    function initialize(address _zapMarket) external initializer {
+        zapMarket = ZapMarketOld(_zapMarket);
+    }
+
+    function deployMedia(
+        string calldata name,
+        string calldata symbol,
+        address marketContractAddr,
+        bool permissive,
+        string calldata _collectionMetadata
+    ) external returns (address) {
+        ZapMediaOld zapMedia = new ZapMediaOld();
+        zapMedia.initialize(
+            name,
+            symbol,
+            marketContractAddr,
+            permissive,
+            _collectionMetadata
+        );
+
+        zapMedia.initTransferOwnership(payable(msg.sender));
+
+        zapMarket.registerMedia(address(zapMedia));
+
+
+        bytes memory name_b = bytes(name);
+        bytes memory symbol_b = bytes(symbol);
+
+        bytes32 name_b32;
+        bytes32 symbol_b32;
+
+        assembly {
+            name_b32 := mload(add(name_b, 32))
+            symbol_b32 := mload(add(symbol_b, 32))
+        }
+
+        zapMarket.configure(msg.sender, address(zapMedia), name_b32, symbol_b32);
+
+        emit MediaDeployed(address(zapMedia));
+
+        return address(zapMedia);
+    }
+}
