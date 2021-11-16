95973aaa15d7a6ce02cfd418f2a8ccbbd7221d54
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2021-11-15 21:41:36 -0500

    Set the external nft name & symbol inside the configureExternalToken function

--- contracts/nft/MediaFactory.sol
+++ contracts/nft/MediaFactory.sol
@@ -5,6 +5,7 @@ pragma experimental ABIEncoderV2;
 import 'hardhat/console.sol';
 
 import {OwnableUpgradeable} from '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
+import {ERC721} from '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 import {IERC721} from '@openzeppelin/contracts/token/ERC721/IERC721.sol';
 import {ZapMedia} from './ZapMedia.sol';
 import {ZapMarket} from './ZapMarket.sol';
@@ -65,8 +66,6 @@ contract MediaFactory is OwnableUpgradeable {
     }
 
     function configureExternalToken(
-        string calldata name,
-        string calldata symbol,
         address tokenAddress,
         uint256 tokenId,
         IMarket.BidShares memory _bidShares
@@ -81,8 +80,8 @@ contract MediaFactory is OwnableUpgradeable {
         }
 
         if (!(zapMarket.isConfigured(tokenAddress))) {
-            bytes memory name_b = bytes(name);
-            bytes memory symbol_b = bytes(symbol);
+            bytes memory name_b = bytes(ERC721(tokenAddress).name());
+            bytes memory symbol_b = bytes(ERC721(tokenAddress).symbol());
 
             bytes32 name_b32;
             bytes32 symbol_b32;
