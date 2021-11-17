d7afc7e2f90b599a7e9be21f8d8ea7657be94515
Jamil Bousquet <jamil.bousquet@gmail.com>
2021-11-17 14:14:43 -0400

    using upgradeable ZapMedia

--- contracts/nft/MediaFactory.sol
+++ contracts/nft/MediaFactory.sol
@@ -1,22 +1,57 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity ^0.8.4;
-pragma experimental ABIEncoderV2;
 
 import {OwnableUpgradeable} from '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
 
+import {MediaProxy} from './MediaProxy.sol';
+import {Ownable} from './Ownable.sol';
 import {ZapMedia} from './ZapMedia.sol';
 import {ZapMarket} from './ZapMarket.sol';
 
+/// @title Zap Media Factory Contract
+/// @notice This contract deploys ZapMedia and external ERC721 contracts,
+///         registers and then configures them to be used on the Zap NFT Marketplace
+/// @dev It creates instances of ERC1976 MediaProxy and sets their implementation to a deployed ZapMedia
 contract MediaFactory is OwnableUpgradeable {
     event MediaDeployed(address indexed mediaContract);
+    event MediaUpdated(address indexed mediaContract);
 
     ZapMarket zapMarket;
+    mapping(address=>address) proxyImplementations;
 
+    /// @notice Contract constructor
+    /// @dev utilises the OZ Initializable contract; cannot be called twice
+    /// @param _zapMarket the address of the ZapMarket contract to register and configure each ERC721 on
     function initialize(address _zapMarket) external initializer {
         zapMarket = ZapMarket(_zapMarket);
     }
 
+    /// @notice Upgrades ZapMedia contract
+    /// @dev calls `upgrateTo` on the MediaProxy contract to upgrade/replace the implementation contract
+    /// @param _proxy a parameter just like in doxygen (must be followed by parameter name)
+    function upgradeMedia(
+        address _proxy,
+        address _newImpl
+    ) external {
+        require(
+            msg.sender != address(0) && msg.sender == ZapMedia(proxyImplementations[_proxy]).getOwner(),
+            "Only the owner can make this upgrade"
+        );
+
+        MediaProxy(_proxy).upgrateTo(_newImpl);
+        emit MediaUpdated(_proxy);
+    }
+
+    /// @notice Deploys ZapMedia ERC721 contracts to be used on ZapMarket
+    /// @dev This is the contract factory function, it deploys a proxy contract, then a ZapMedia contract,
+    ///      and then sets the implementation and initializes ZapMedia
+    /// @param name name of the collection
+    /// @param symbol collection's symbol
+    /// @param marketContractAddr ZapMarket contract to attach to, this can not be updated
+    /// @param permissive whether or not you would like this contract to be minted by everyone or just the owner
+    /// @param _collectionMetadata the metadata URI of the collection
+    /// @return the address of the deployed ZapMedia proxy
     function deployMedia(
         string calldata name,
         string calldata symbol,
@@ -24,18 +59,15 @@ contract MediaFactory is OwnableUpgradeable {
         bool permissive,
         string calldata _collectionMetadata
     ) external returns (address) {
+        MediaProxy proxy = new MediaProxy();
         ZapMedia zapMedia = new ZapMedia();
-        zapMedia.initialize(
-            name,
-            symbol,
-            marketContractAddr,
-            permissive,
-            _collectionMetadata
-        );
 
-        zapMedia.initTransferOwnership(payable(msg.sender));
+        proxy.initialize(address(zapMedia), payable(msg.sender), name, symbol, marketContractAddr, permissive, _collectionMetadata);
+        address proxyAddress = address(proxy);
 
-        zapMarket.registerMedia(address(zapMedia));
+        proxyImplementations[proxyAddress] = address(zapMedia);
+
+        zapMarket.registerMedia(proxyAddress);
 
 
         bytes memory name_b = bytes(name);
@@ -49,10 +81,10 @@ contract MediaFactory is OwnableUpgradeable {
             symbol_b32 := mload(add(symbol_b, 32))
         }
 
-        zapMarket.configure(msg.sender, address(zapMedia), name_b32, symbol_b32);
+        zapMarket.configure(msg.sender, proxyAddress, name_b32, symbol_b32);
 
-        emit MediaDeployed(address(zapMedia));
+        emit MediaDeployed(proxyAddress);
 
-        return address(zapMedia);
+        return proxyAddress;
     }
 }
---
+++ contracts/nft/MediaProxy.sol
@@ -0,0 +1,135 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
+pragma experimental ABIEncoderV2;
+
+import {ERC1967UpgradeUpgradeable} from '@openzeppelin/contracts-upgradeable/proxy/ERC1967/ERC1967UpgradeUpgradeable.sol';
+import "@openzeppelin/contracts-upgradeable/utils/AddressUpgradeable.sol";
+
+/// @title Upgradeable proxy contract for a ZapMedia
+/// @notice This contract acts as a proxy that delegates calls for ZapMedia.
+///         it stores the ZapMedia implementation address which can be upgraded by the admin
+/// @dev Implements the OpenZeppelin ERC1967UpgradeUpgradeable contract
+contract MediaProxy is ERC1967UpgradeUpgradeable {
+
+    modifier onlyAdmin() {
+        require(msg.sender == _getAdmin(), "Only the Admin can call this function");
+        _;
+    }
+
+    /// @notice Constructor for the ZapMedia proxy and its implementation
+    /// @dev This function is initializable and implements the OZ Initializable contract by inheiritance
+    /// @param implementation the address of the uninitialized ZapMedia contract
+    /// @param owner the intended owner of the ZapMedia contract
+    /// @param name name of the collection
+    /// @param symbol collection's symbol
+    /// @param marketContractAddr ZapMarket contract to attach to, this can not be updated
+    /// @param permissive whether or not you would like this contract to be minted by everyone or just the owner
+    /// @param collectionURI the metadata URI of the collection
+    function initialize(
+        address implementation,
+        address payable owner,
+        string calldata name,
+        string calldata symbol,
+        address marketContractAddr,
+        bool permissive,
+        string calldata collectionURI
+    ) public {
+        _upgradeToAndCall(
+            implementation,
+            abi.encodeWithSignature(
+                "initialize(string,string,address,bool,string)",
+                name,
+                symbol,
+                marketContractAddr,
+                permissive,
+                collectionURI
+            ),
+            false
+        );
+
+        _changeAdmin(msg.sender);
+
+        (bool success, bytes memory returndata) = implementation.delegatecall(
+            abi.encodeWithSignature("initTransferOwnership(address)", owner)
+        );
+
+        require(
+            success && returndata.length == 0,
+            "Creating ZapMedia proxy: Can not transfer ownership of proxy"
+        );
+
+    }
+
+    /// @notice Changes the admin contract of this ERC1967 contract
+    /// @dev This is a wrapper function for the ERC1967 _changeAdmin function and uses a modifier
+    ///      to ensure that only the current admin contract can change the admin
+    /// @param newAdmin the address of the new admin contract
+    function changeAdmin(address newAdmin) external onlyAdmin {
+        _changeAdmin(newAdmin);
+    }
+
+    /// @notice Upgrades the ERC1967 Proxy to a new implementation
+    /// @dev This is a wrapper function for the ERC1967 _upgradeTo function and uses a modifier
+    ///      to ensure that only the current admin contract can change the admin
+    /// @param _impl the address of the new implementaion contract
+    function upgrateTo(address _impl) external onlyAdmin {
+        _upgradeTo(_impl);
+    }
+
+    /// @notice Upgrades the ERC1967 Proxy to a new implementation and immediately calls a function on it
+    /// @dev This is a wrapper function for the ERC1967 _upgradeToAndCall function and uses a modifier
+    ///      to ensure that only the current admin contract can change the admin
+    /// @param _impl the address of the new implementaion contract
+    /// @param data calldata containing function signature and params to call once `_impl` is updated
+    function upgradeToAndCall(address _impl, bytes memory data) external onlyAdmin{
+        _upgradeToAndCall(_impl, data, false);
+    }
+
+    /// @notice Get the owner of the ZapMedia implementation
+    /// @dev This makes a `delegatecall`, which means that __the `owner` is stored in this proxy's state__
+    /// @param _impl address of the implementation which has the getOwner function bytecode
+    /// @return _implOwner address of this ZapMedia's owner
+    function getImplOwner(address _impl) public onlyAdmin returns (address _implOwner){
+        (bool success, bytes memory returndata) = _impl.delegatecall(
+            abi.encodeWithSignature("getOwner()")
+        );
+
+        require(success && returndata.length > 0, "Can not get the owner of this ZapMedia");
+        _implOwner = abi.decode(returndata, (address));
+    }
+
+    function _delegate(address _impl) internal virtual {
+        assembly {
+            calldatacopy(0, 0, calldatasize())
+
+            let result := delegatecall(
+                gas(),
+                _impl,
+                0,
+                calldatasize(),
+                0,
+                0
+            )
+
+            // Copy the returned data.
+            returndatacopy(0, 0, returndatasize())
+
+            switch result
+                // delegatecall returns 0 on error.
+                case 0 {
+                    revert(0, returndatasize())
+                }
+                default {
+                    return(0, returndatasize())
+                }
+            }
+    }
+
+    fallback() external {
+        // ensures that the call is always made to the implementation (ZapMedia)
+        // https://docs.openzeppelin.com/upgrades-plugins/1.x/proxies#transparent-proxies-and-function-clashes 
+        if (msg.sender != _getAdmin()){
+            _delegate(_getImplementation());
+        }
+    }
+}
