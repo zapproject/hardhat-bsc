52c06f4c7919148914280062806627c9726f6393
Jamil Bousquet <jamil.bousquet@gmail.com>
2021-11-18 10:05:02 -0400

    using hotfix for contract too large error

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -85,7 +85,10 @@ const config = {
       //accounts: { mnemonic: process.env.MNEMONIC }
     },
     hardhat: {
-      gasPrice: 8000000000
+      gas: 12000000,
+      gasPrice: 10000000000,
+      blockGasLimit: 0x1fffffffffffff,
+      allowUnlimitedContractSize: true,
     },
     rinkeby: {
       url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
