04f62d1225483aab728b2c32c3a0f0c9d2c352da
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2021-11-18 10:58:17 -0500

    Tests passing but contract code size is over 24576 bytes

--- contracts/nft/MediaFactory.sol
+++ contracts/nft/MediaFactory.sol
@@ -2,19 +2,18 @@
 
 pragma solidity ^0.8.4;
 pragma experimental ABIEncoderV2;
-import 'hardhat/console.sol';
 
 import {OwnableUpgradeable} from '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
 import {IERC721} from '@openzeppelin/contracts/token/ERC721/IERC721.sol';
+import {ERC721} from '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 import {ZapMedia} from './ZapMedia.sol';
 import {ZapMarket} from './ZapMarket.sol';
 import {IMarket} from './interfaces/IMarket.sol';
-
-// import {External} from './External.sol';
+import 'hardhat/console.sol';
 
 contract MediaFactory is OwnableUpgradeable {
     event MediaDeployed(address indexed mediaContract);
-    // event ExternalTokenDeployed(address indexed extToken);
+    event ExternalTokenDeployed(address indexed extToken);
 
     ZapMarket zapMarket;
 
@@ -64,5 +63,52 @@ contract MediaFactory is OwnableUpgradeable {
         emit MediaDeployed(address(zapMedia));
 
         return address(zapMedia);
+    }
+
+    function configureExternalToken(
+        address tokenAddress,
+        uint256 tokenId,
+        IMarket.BidShares memory _bidShares
+    ) external returns (bool success) {
+        require(
+            ERC721(tokenAddress).ownerOf(tokenId) == msg.sender,
+            'MediaFactory: Only token owner can configure to ZapMarket'
+        );
+
+        if (!(zapMarket.isRegistered(tokenAddress))) {
+            zapMarket.registerMedia(tokenAddress);
+        } else {
+            require(
+                zapMarket.isInternal(tokenAddress),
+                'This operation is meant for external NFTs'
+            );
+        }
+
+        if (!(zapMarket.isConfigured(tokenAddress))) {
+            bytes memory name_b = bytes(ERC721(tokenAddress).name());
+            bytes memory symbol_b = bytes(ERC721(tokenAddress).symbol());
+
+            bytes32 name_b32;
+            bytes32 symbol_b32;
+
+            assembly {
+                name_b32 := mload(add(name_b, 32))
+                symbol_b32 := mload(add(symbol_b, 32))
+            }
+
+            zapMarket.configure(
+                msg.sender,
+                tokenAddress,
+                name_b32,
+                symbol_b32,
+                false
+            );
+        }
+
+        zapMarket.setBidShares(tokenAddress, tokenId, _bidShares);
+
+        emit ExternalTokenDeployed(tokenAddress);
+
+        return true;
     }
 }
