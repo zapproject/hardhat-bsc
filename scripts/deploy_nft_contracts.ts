f282a8cf1d76713754c448962ddd92053ccecb46
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2021-11-24 19:10:59 -0500

    Setup initialization

--- scripts/deploy_nft_contracts.ts
+++ scripts/deploy_nft_contracts.ts
@@ -178,22 +178,31 @@ async function main() {
 
     const MediaFactory = await ethers.getContractFactory("MediaFactory", signers[0]);
 
-    const mediaFactory = await upgrades.deployProxy(
-        MediaFactory,
-        ['0x8215bd8eAa4fff887CCf31F7A38e93e38c829F15', '0xEfA96E2C4B04b180aEAF0dABECf5f60a7e03b115'],
-        { initializer: 'initialize' }
-    ) as MediaFactory;
+    const mediaFactory = await MediaFactory.attach('0x53813994415203448cdF5b2113d82b9D0058A23C')
+
+    await mediaFactory.initialize(
+        '0x8215bd8eAa4fff887CCf31F7A38e93e38c829F15',
+        '0xEfA96E2C4B04b180aEAF0dABECf5f60a7e03b115'
+    );
+
+    console.log("MediaFactory initialized at:", mediaFactory.address)
+
+    // const mediaFactory = await upgrades.deployProxy(
+    //     MediaFactory,
+    //     ['0x8215bd8eAa4fff887CCf31F7A38e93e38c829F15', '0xEfA96E2C4B04b180aEAF0dABECf5f60a7e03b115'],
+    //     { initializer: 'initialize' }
+    // ) as MediaFactory;
 
 
-    const zapMarket = await zapMarketFactory.attach('0x8215bd8eAa4fff887CCf31F7A38e93e38c829F15');
+    // const zapMarket = await zapMarketFactory.attach('0x8215bd8eAa4fff887CCf31F7A38e93e38c829F15');
 
-    // set mediaFactory address to ZapMarket
-    await zapMarket.setMediaFactory(mediaFactory.address);
-    console.log("MediaFactory set to ZapMarket");
+    // // set mediaFactory address to ZapMarket
+    // await zapMarket.setMediaFactory(mediaFactory.address);
+    // console.log("MediaFactory set to ZapMarket");
 
-    await mediaFactory.deployed();
-    console.log('MediaFactory deployed to:', mediaFactory.address);
-    console.log("MediaFactory Owner: ", await mediaFactory.owner(), "\n")
+    // await mediaFactory.deployed();
+    // console.log('MediaFactory deployed to:', mediaFactory.address);
+    // console.log("MediaFactory Owner: ", await mediaFactory.owner(), "\n")
 
 
     // const mediaDeployGas = await mediaFactory.estimateGas.deployMedia(
