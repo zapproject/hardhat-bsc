4b12cdc60011963e53568227a9bfe5c152d8fe07
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2021-12-01 17:00:57 -0500

    Create the External nft test file

---
+++ test/ExternalNftTests.ts
@@ -0,0 +1,20 @@
+import { ethers, upgrades } from 'hardhat';
+
+import { ZapMarket, ZapMedia } from '../typechain';
+
+import { solidity } from 'ethereum-waffle';
+
+import chai, { expect } from 'chai';
+
+const { deployments } = require('hardhat');
+
+
+describe("Testing", () => {
+
+    it('testing 1 2 3', async function () {
+
+    });
+
+
+});
+
