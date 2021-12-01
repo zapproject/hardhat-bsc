afd73a740632e127a1ee18d5fe6ab74148db74b3
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2021-12-01 18:08:15 -0500

    Error with proxy admin

--- test/ExternalNftTests.ts
+++ test/ExternalNftTests.ts
@@ -1,6 +1,6 @@
 import { ethers, upgrades } from 'hardhat';
 
-import { ZapMarket, ZapMedia, ZapVault } from '../typechain';
+import { ZapMarket, ZapMedia, ZapVault, ZapMarketV2 } from '../typechain';
 
 import { solidity } from 'ethereum-waffle';
 
@@ -15,15 +15,23 @@ describe("Testing", () => {
 
     beforeEach(async () => {
 
+        const signers = await ethers.getSigners()
         await deployments.fixture();
 
         const vaultFactory = await deployments.get('ZapVault');
+        const zapMarketFactory = await deployments.get('ZapMarket')
 
         zapVault = await ethers.getContractAt('ZapVault', vaultFactory.address) as ZapVault;
+        const zapMarket = await ethers.getContractAt('ZapMarket', zapMarketFactory.address)
+
+        const ZapMarketV2 = await ethers.getContractFactory('ZapMarketV2', signers[0])
+
+        const zapMarketV2 = await upgrades.upgradeProxy(zapMarket.address, ZapMarketV2)
+
 
     })
 
-    it('testing 1 2 3', async function () {
+    it('testing', async function () {
 
 
     });
