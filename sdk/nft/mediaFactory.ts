2901b5ecaf1d4db5a11eb40c7c57ce107f8b842f
fullDev9233 <jimyy9211@gmail.com>
2021-12-20 13:44:38 -0500

    add mediaFactory

--- sdk/nft/mediaFactory.ts
+++ sdk/nft/mediaFactory.ts
@@ -1,29 +1,63 @@
-import { Signer } from 'ethers';
-import deployMediaFunc from '.';
+import { Contract, ethers, Signer } from 'ethers';
+import { mediaFactoryAddresses, zapMarketAddresses } from './addresses';
+import { mediaFactoryAbi } from './abi';
+
+let mediaFactoryAddress: string;
+let zapMarketAddress: string;
+
+function contractAddresses(networkId: number) {
+  switch (networkId) {
+    // Localhost
+    case 31337:
+      mediaFactoryAddress = mediaFactoryAddresses.localhost;
+      zapMarketAddress = zapMarketAddresses.localhost;
+      console.log('localhost');
+      break;
+
+    // Rinkeby
+    case 4:
+      mediaFactoryAddress = mediaFactoryAddresses.rinkeby;
+      zapMarketAddress = zapMarketAddresses.rinkeby;
+      console.log('Rinkeby');
+      break;
+  }
+
+  return {
+    mediaFactoryAddress,
+    zapMarketAddress,
+  };
+}
 
 class MediaFactory {
-  async deployMedia(
-    networkId: number,
-    signer: Signer,
+  contract: Contract;
+  networkId: number;
+  signer: Signer;
+
+  constructor(networkId: number, signer: Signer) {
+    this.networkId = networkId;
+    this.signer = signer;
+    this.contract = new ethers.Contract(
+      contractAddresses(networkId).mediaFactoryAddress,
+      mediaFactoryAbi,
+      signer,
+    )
+  }
+
+  deployMedia(
     collectionName: string,
     collectionSymbol: string,
     permissive: boolean,
-    collectionMetadta: string,
-  ) {
-    try {
-      const result = await deployMediaFunc(
-        networkId,
-        signer,
-        collectionName,
-        collectionSymbol,
-        permissive,
-        collectionMetadta,
-      );
-      return result;
-    } catch (err) {
-      console.log(err);
-    }
-  }
+    collectionMetadta: string
+  ): Promise<void> {
+    return this.contract.deployMedia(
+      collectionName,
+      collectionSymbol,
+      contractAddresses(this.networkId).zapMarketAddress,
+      permissive,
+      collectionMetadta,
+    )
+  };
+
 }
 
 export default MediaFactory;
