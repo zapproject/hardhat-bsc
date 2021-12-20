e4f31be3099d8d1b1c2dbb297a25596b6a189eaa
fullDev9233 <jimyy9211@gmail.com>
2021-12-20 15:13:49 -0500

    add 2 tests

--- sdk/nft/test/mediaFactory.test.ts
+++ sdk/nft/test/mediaFactory.test.ts
@@ -3,7 +3,10 @@ import { ethers, } from 'ethers';
 import MediaFactory from '../mediaFactory';
 
 const networkId = 4;
-const signer = new ethers.providers.JsonRpcProvider();
+const provider = new ethers.providers.JsonRpcProvider('http://localhost:8545');
+const privateKey = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
+
+const signer = new ethers.Wallet(privateKey, provider);
 
 describe("MediaFactory", () => {
   let collectionName: string;
@@ -11,8 +14,13 @@ describe("MediaFactory", () => {
   let permissive: boolean;
   let collectionMetadta: string;
 
+  it('should be a class', () => {
+    const mediaFactory = new MediaFactory(networkId, signer);
+    console.log(mediaFactory);
+  });
+
   it('Should be able to deploy a new Media contract', async () => {
-    const mediaFactory = new MediaFactory(networkId, signer.getSigner());
+    const mediaFactory = new MediaFactory(networkId, signer);
     await mediaFactory.deployMedia(collectionName, collectionSymbol, permissive, collectionMetadta);
   })
 });
