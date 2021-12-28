c227171ca429b807c41813f11500d7e7c7768c47
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2021-12-28 15:18:43 -0500

    Able to deploy  a media contract from the MediaFactory class

--- sdk/nft/test/mediaFactory.test.ts
+++ sdk/nft/test/mediaFactory.test.ts
@@ -1,26 +1,49 @@
-import chai from 'chai';
-import { ethers, } from 'ethers';
+import { expect } from 'chai';
+import { ethers } from 'ethers';
+import { zapMediaAbi } from '../abi';
+import { eventNames } from 'process';
 import MediaFactory from '../mediaFactory';
 
-const networkId = 4;
-const provider = new ethers.providers.JsonRpcProvider('http://localhost:8545');
-const privateKey = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
 
-const signer = new ethers.Wallet(privateKey, provider);
+const provider = new ethers.providers.JsonRpcProvider('http://localhost:8545');
 
 describe("MediaFactory", () => {
-  let collectionName: string;
-  let collectionSymbol: string;
-  let permissive: boolean;
-  let collectionMetadta: string;
 
-  it('should be a class', () => {
-    const mediaFactory = new MediaFactory(networkId, signer);
-    console.log(mediaFactory);
+  let signer0: any
+  let mediaFactory: any
+  // let deployMedia: any
+  let testCollectionAddress: string;
+
+  beforeEach(async () => {
+
+    signer0 = await provider.getSigner(0);
+
+    // Creates 
+    mediaFactory = new MediaFactory(31337, signer0);
+
+  })
+
+  it("Should be able to deploy a Media collection", async () => {
+
+    // 
+    const deployMedia = await mediaFactory.deployMedia(
+      "Test Collection",
+      "TC",
+      false,
+      "ipfs://testing"
+    );
+
+    const receipt = await deployMedia.wait();
+
+    const eventLog = receipt.events[receipt.events.length - 1];
+
+    // The event name is "MediaDeployed"
+    const eventName = eventLog.event;
+
+    testCollectionAddress = eventLog.args.mediaContract;
+
+    expect(eventName).to.equal('MediaDeployed');
+
   });
 
-  it('Should be able to deploy a new Media contract', async () => {
-    const mediaFactory = new MediaFactory(networkId, signer);
-    await mediaFactory.deployMedia(collectionName, collectionSymbol, permissive, collectionMetadta);
-  })
 });
