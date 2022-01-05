724bfc003bebe06c275654f321837426403f1a6f
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2022-01-05 15:53:07 -0500

    Adding view & wriete functions to the zapMedia class.

--- sdk/nft/test/zapMedia.test.ts
+++ sdk/nft/test/zapMedia.test.ts
@@ -13,181 +13,222 @@ const contracts = require('../deploy.js');
 const provider = new ethers.providers.JsonRpcProvider('http://localhost:8545');
 
 describe('ZapMedia', () => {
-    let bidShares: any;
-    let mediaData: any;
-    let token: any;
-    let zapVault: any;
-    let zapMarket: any;
-    let zapMediaImpl: any;
-    let mediaFactory: any;
-    let signer: any;
-    let zapMedia: any;
+  let bidShares: any;
+  let mediaData: any;
+  let token: any;
+  let zapVault: any;
+  let zapMarket: any;
+  let zapMediaImpl: any;
+  let mediaFactory: any;
+  let signer: any;
+  let zapMedia: any;
 
-    beforeEach(async () => {
-        signer = provider.getSigner(0);
+  beforeEach(async () => {
+    signer = provider.getSigner(0);
 
-        token = await contracts.deployZapToken();
-        zapVault = await contracts.deployZapVault();
-        zapMarket = await contracts.deployZapMarket();
-        zapMediaImpl = await contracts.deployZapMediaImpl();
-        mediaFactory = await contracts.deployMediaFactory();
-        zapMedia = await contracts.deployZapMedia();
+    token = await contracts.deployZapToken();
+    zapVault = await contracts.deployZapVault();
+    zapMarket = await contracts.deployZapMarket();
+    zapMediaImpl = await contracts.deployZapMediaImpl();
+    mediaFactory = await contracts.deployMediaFactory();
+    zapMedia = await contracts.deployZapMedia();
 
-        zapMarketAddresses['1337'] = zapMarket.address;
-        mediaFactoryAddresses['1337'] = mediaFactory.address;
-        zapMediaAddresses['1337'] = zapMedia.address;
+    zapMarketAddresses['1337'] = zapMarket.address;
+    mediaFactoryAddresses['1337'] = mediaFactory.address;
+    zapMediaAddresses['1337'] = zapMedia.address;
+  });
+
+  describe('#constructor', () => {
+    it('Should throw an error if the networkId is invalid', async () => {
+      expect(() => {
+        new ZapMedia(300, signer);
+      }).to.throw('ZapMedia Constructor: Network Id is not supported.');
     });
+  });
 
-    describe('#constructor', () => {
-        it('Should throw an error if the networkId is invalid', async () => {
-            expect(() => {
-                new ZapMedia(300, signer);
-            }).to.throw('ZapMedia Constructor: Network Id is not supported.');
-        });
-    });
+  describe('contract Functions', () => {
+    describe('Write Functions', () => {
+      let tokenURI =
+        'https://bafkreievpmtbofalpowrcbr5oaok33e6xivii62r6fxh6fontaglngme2m.ipfs.dweb.link/';
+      let metadataURI =
+        'https://bafkreihhu7xo7knc3vn42jj26gz3jkvh3uu3rwurkb4djsoo5ayqs2s25a.ipfs.dweb.link/';
+      beforeEach(async () => {
+        let metadataHex = ethers.utils.formatBytes32String('Test');
+        let metadataHashRaw = ethers.utils.keccak256(metadataHex);
+        let metadataHashBytes = ethers.utils.arrayify(metadataHashRaw);
 
-    describe('contract Functions', () => {
-        describe('Write Functions', () => {
-            let tokenURI =
-                'https://bafkreievpmtbofalpowrcbr5oaok33e6xivii62r6fxh6fontaglngme2m.ipfs.dweb.link/';
-            let metadataURI =
-                'https://bafkreihhu7xo7knc3vn42jj26gz3jkvh3uu3rwurkb4djsoo5ayqs2s25a.ipfs.dweb.link/';
-            beforeEach(async () => {
-                let metadataHex = ethers.utils.formatBytes32String('Test');
-                let metadataHashRaw = ethers.utils.keccak256(metadataHex);
-                let metadataHashBytes = ethers.utils.arrayify(metadataHashRaw);
+        let contentHex = ethers.utils.formatBytes32String('Test Car');
+        let contentHashRaw = ethers.utils.keccak256(contentHex);
+        let contentHashBytes = ethers.utils.arrayify(contentHashRaw);
 
-                let contentHex = ethers.utils.formatBytes32String('Test Car');
-                let contentHashRaw = ethers.utils.keccak256(contentHex);
-                let contentHashBytes = ethers.utils.arrayify(contentHashRaw);
+        let contentHash = contentHashBytes;
+        let metadataHash = metadataHashBytes;
 
-                let contentHash = contentHashBytes;
-                let metadataHash = metadataHashBytes;
+        mediaData = constructMediaData(tokenURI, metadataURI, contentHash, metadataHash);
 
-                mediaData = constructMediaData(tokenURI, metadataURI, contentHash, metadataHash);
+        bidShares = constructBidShares(
+          [
+            await provider.getSigner(1).getAddress(),
+            await provider.getSigner(2).getAddress(),
+            await provider.getSigner(3).getAddress(),
+          ],
+          [15, 15, 15],
+          15,
+          35,
+        );
+      });
 
-                bidShares = constructBidShares(
-                    [
-                        await provider.getSigner(1).getAddress(),
-                        await provider.getSigner(2).getAddress(),
-                        await provider.getSigner(3).getAddress(),
-                    ],
-                    [15, 15, 15],
-                    15,
-                    35,
-                );
-            });
+      describe('#updateContentURI', () => {
+        it('Should thrown an error if the tokenURI does not begin with `https://`', async () => {
+          //   Instantiates the ZapMedia class
+          const media = new ZapMedia(1337, signer);
 
-            describe('#updateContentURI', () => {
-                it('Should thrown an error if the tokenURI does not begin with `https://`', async () => {
-                    //   Instantiates the ZapMedia class
-                    const media = new ZapMedia(1337, signer);
+          mediaData.tokenURI = 'http://example.com';
 
-                    mediaData.tokenURI = 'http://example.com';
-
-                    await media
-                        .mint(mediaData, bidShares)
-                        .then((res) => {
-                            return res;
-                        })
-                        .catch((err) => {
-                            expect(err).to.equal(
-                                'Invariant failed: http://example.com must begin with `https://`',
-                            );
-                        });
-                });
-
-                it('Should throw an error if the updateContentURI tokenId does not exist', async () => {
-                    //   Instantiates the ZapMedia class
-                    const media = new ZapMedia(1337, signer);
-
-                    await media
-                        .updateContentURI(0, 'www.newURI.com')
-                        .then((res) => {
-                            console.log(res);
-                        })
-                        .catch((err) => {
-                            expect(err.message).to.equal(
-                                'Invariant failed: ZapMedia (updateContentURI): TokenId does not exist.',
-                            );
-                        });
-                });
-
-                it('Should throw an error if the fetchContentURI tokenId does not exist', async () => {
-                    //   Instantiates the ZapMedia class
-                    const media = new ZapMedia(1337, signer);
-
-                    await media
-                        .fetchContentURI(0)
-                        .then((res) => {
-                            console.log(res);
-                        })
-                        .catch((err) => {
-                            expect(err.message).to.equal(
-                                'Invariant failed: ZapMedia (fetchContentURI): TokenId does not exist.',
-                            );
-                        });
-                });
-
-                it('Should update the content uri', async () => {
-                    //   Instantiates the ZapMedia class
-                    const media = new ZapMedia(1337, signer);
-
-                    // Mints tokenId 0
-                    await media.mint(mediaData, bidShares);
-
-                    // Returns tokenId 0's tokenURI
-                    const fetchTokenURI = await media.fetchContentURI(0);
-
-                    // The returned tokenURI should equal the tokenURI configured in the mediaData
-                    expect(fetchTokenURI).to.equal(mediaData.tokenURI);
-
-                    // Updates tokenId 0's tokenURI
-                    await media.updateContentURI(0, 'https://newURI.com');
-
-                    // Returns tokenId 0's tokenURI
-                    const fetchNewURI = await media.fetchContentURI(0);
-
-                    // The new tokenURI returned should equal the updatedURI
-                    expect(fetchNewURI).to.equal('https://newURI.com');
-                });
-            });
-
-            describe('#updateMetadataURI', () => {
-
-                it('Should thrown an error if the metadataURI does not begin with `https://`', async () => {
-                    //   Instantiates the ZapMedia class
-                    const media = new ZapMedia(1337, signer);
-
-                    mediaData.metadataURI = 'http://example.com';
-
-                    await media
-                        .mint(mediaData, bidShares)
-                        .then((res) => {
-                            return res;
-                        })
-                        .catch((err) => {
-                            expect(err).to.equal(
-                                'Invariant failed: http://example.com must begin with `https://`',
-                            );
-                        });
-                });
-
-                it('Should update the metadata uri', async () => {
-                    const media = new ZapMedia(1337, signer);
-
-                    await media.mint(mediaData, bidShares);
-
-                    const fetchMetadataURI = await media.fetchMetadataURI(0);
-                    expect(fetchMetadataURI).to.equal(mediaData.metadataURI);
-
-                    await media.updateMetadataURI(0, 'https://newMetadataURI.com');
-
-                    const newMetadataURI = await media.fetchMetadataURI(0);
-                    expect(newMetadataURI).to.equal('https://newMetadataURI.com');
-
-                })
+          await media
+            .mint(mediaData, bidShares)
+            .then((res) => {
+              return res;
             })
+            .catch((err) => {
+              expect(err).to.equal(
+                'Invariant failed: http://example.com must begin with `https://`',
+              );
+            });
         });
+
+        it('Should throw an error if the updateContentURI tokenId does not exist', async () => {
+          //   Instantiates the ZapMedia class
+          const media = new ZapMedia(1337, signer);
+
+          await media
+            .updateContentURI(0, 'www.newURI.com')
+            .then((res) => {
+              console.log(res);
+            })
+            .catch((err) => {
+              expect(err.message).to.equal(
+                'Invariant failed: ZapMedia (updateContentURI): TokenId does not exist.',
+              );
+            });
+        });
+
+        it('Should throw an error if the fetchContentURI tokenId does not exist', async () => {
+          //   Instantiates the ZapMedia class
+          const media = new ZapMedia(1337, signer);
+
+          await media
+            .fetchContentURI(0)
+            .then((res) => {
+              console.log(res);
+            })
+            .catch((err) => {
+              expect(err.message).to.equal(
+                'Invariant failed: ZapMedia (fetchContentURI): TokenId does not exist.',
+              );
+            });
+        });
+
+        it('Should update the content uri', async () => {
+          //   Instantiates the ZapMedia class
+          const media = new ZapMedia(1337, signer);
+
+          // Mints tokenId 0
+          await media.mint(mediaData, bidShares);
+
+          // Returns tokenId 0's tokenURI
+          const fetchTokenURI = await media.fetchContentURI(0);
+
+          // The returned tokenURI should equal the tokenURI configured in the mediaData
+          expect(fetchTokenURI).to.equal(mediaData.tokenURI);
+
+          // Updates tokenId 0's tokenURI
+          await media.updateContentURI(0, 'https://newURI.com');
+
+          // Returns tokenId 0's tokenURI
+          const fetchNewURI = await media.fetchContentURI(0);
+
+          // The new tokenURI returned should equal the updatedURI
+          expect(fetchNewURI).to.equal('https://newURI.com');
+        });
+      });
+
+      describe('#updateMetadataURI', () => {
+        it('Should thrown an error if the metadataURI does not begin with `https://`', async () => {
+          //   Instantiates the ZapMedia class
+          const media = new ZapMedia(1337, signer);
+
+          mediaData.metadataURI = 'http://example.com';
+
+          await media
+            .mint(mediaData, bidShares)
+            .then((res) => {
+              return res;
+            })
+            .catch((err) => {
+              expect(err).to.equal(
+                'Invariant failed: http://example.com must begin with `https://`',
+              );
+            });
+        });
+
+        it('Should update the metadata uri', async () => {
+          const media = new ZapMedia(1337, signer);
+
+          await media.mint(mediaData, bidShares);
+
+          const fetchMetadataURI = await media.fetchMetadataURI(0);
+          expect(fetchMetadataURI).to.equal(mediaData.metadataURI);
+
+          await media.updateMetadataURI(0, 'https://newMetadataURI.com');
+
+          const newMetadataURI = await media.fetchMetadataURI(0);
+          expect(newMetadataURI).to.equal('https://newMetadataURI.com');
+        });
+      });
+
+      describe.only('#mint', () => {
+        it('throws an error if bid shares do not sum to 100', async () => {
+          let bidShareSum = 0;
+          const media = new ZapMedia(1337, signer);
+
+          bidShares.creator.value = bidShares.creator.value.add(BigInt(1e18));
+
+          for (var i = 0; i < bidShares.collabShares.length; i++) {
+            bidShareSum += parseInt(bidShares.collabShares[i]);
+          }
+
+          bidShareSum += parseInt(bidShares.creator.value) + parseInt(bidShares.owner.value) + 5e18;
+
+          await media
+            .mint(mediaData, bidShares)
+            .then((res) => {
+              return res;
+            })
+            .catch((err) => {
+              expect(err).to.equal(
+                `Invariant failed: The BidShares sum to ${bidShareSum}, but they must sum to 100000000000000000000`,
+              );
+            });
+        });
+
+        it('Should be able to mint', async () => {
+          const media = new ZapMedia(1337, signer);
+
+          const preTotalSupply = (await media.fetchTotalMedia()).toNumber();
+
+          expect(preTotalSupply).to.equal(0);
+
+          await media.mint(mediaData, bidShares);
+
+          const owner = await media.fetchOwnerOf(0);
+          const onChainContentHash = await media.fetchContentHash(0);
+          const onChainMetadataHash = await media.fetchMetadataHash(0);
+          //   const onChainBidShares = await media.fetchCurrentBidShares(0);
+          const onChainContentURI = await media.fetchContentURI(0);
+          const onChainMetadataURI = await media.fetchMetadataURI(0);
+        });
+      });
     });
+  });
 });
--- sdk/nft/zapMedia.ts
+++ sdk/nft/zapMedia.ts
@@ -1,11 +1,11 @@
 import {
-    Contract,
-    ContractTransaction,
-    BigNumber,
-    BigNumberish,
-    ethers,
-    Signer,
-    Wallet,
+  Contract,
+  ContractTransaction,
+  BigNumber,
+  BigNumberish,
+  ethers,
+  Signer,
+  Wallet,
 } from 'ethers';
 
 import { contractAddresses, Decimal, validateBidShares, validateURI } from './utils';
@@ -17,109 +17,139 @@ import { MediaData, BidShares } from './types';
 import invariant from 'tiny-invariant';
 
 class ZapMedia {
-    networkId: number;
-    mediaIndex: any;
-    contract: any;
-    signer: Signer;
+  networkId: number;
+  mediaIndex: any;
+  contract: any;
+  signer: Signer;
 
-    constructor(networkId: number, signer: Signer, mediaIndex?: number) {
-        this.networkId = networkId;
+  constructor(networkId: number, signer: Signer, mediaIndex?: number) {
+    this.networkId = networkId;
 
-        this.signer = signer;
+    this.signer = signer;
 
-        this.mediaIndex = mediaIndex;
+    this.mediaIndex = mediaIndex;
 
-        if (mediaIndex === undefined) {
-            this.contract = new ethers.Contract(
-                contractAddresses(networkId).zapMediaAddress,
-                zapMediaAbi,
-                signer,
-            );
-        } else {
-        }
+    if (mediaIndex === undefined) {
+      this.contract = new ethers.Contract(
+        contractAddresses(networkId).zapMediaAddress,
+        zapMediaAbi,
+        signer,
+      );
+    } else {
     }
+  }
 
-    /*********************
-     * Zap View Methods
-     *********************
-     */
+  /*********************
+   * Zap View Methods
+   *********************
+   */
 
-    /**
-     * Fetches the content hash for the specified media on the Zap Media Contract
-     * @param mediaId
-     */
-    public async fetchContentHash(mediaId: BigNumberish): Promise<string> {
-        return await this.contract.tokenContentHashes(mediaId);
+  public async fetchBalanceOf(owner: string): Promise<BigNumber> {
+    return this.contract.balanceOf(owner);
+  }
+
+  /**
+   * Fetches the owner of the specified media on an instance of the Zap Media Contract
+   * @param mediaId
+   */
+  public async fetchOwnerOf(mediaId: BigNumberish): Promise<string> {
+    return this.contract.ownerOf(mediaId);
+  }
+
+  /**
+   * Fetches the content hash for the specified media on the Zap Media Contract
+   * @param mediaId
+   */
+  public async fetchContentHash(mediaId: BigNumberish): Promise<string> {
+    return await this.contract.tokenContentHashes(mediaId);
+  }
+
+  /**
+   * Fetches the metadata hash for the specified media on an instance of the Zap Media Contract
+   * @param mediaId
+   */
+  public async fetchMetadataHash(mediaId: BigNumberish): Promise<string> {
+    return this.contract.tokenMetadataHashes(mediaId);
+  }
+
+  /**
+   * Fetches the content uri for the specified media on an instance of the Zap Media Contract
+   * @param mediaId
+   */
+  public async fetchContentURI(mediaId: BigNumberish): Promise<string> {
+    try {
+      return await this.contract.tokenURI(mediaId);
+    } catch {
+      invariant(false, 'ZapMedia (fetchContentURI): TokenId does not exist.');
     }
+  }
 
-    /**
-     * Fetches the content uri for the specified media on an instance of the Zap Media Contract
-     * @param mediaId
-     */
-    public async fetchContentURI(mediaId: BigNumberish): Promise<string> {
-        try {
-            return await this.contract.tokenURI(mediaId);
-        } catch {
-            invariant(false, 'ZapMedia (fetchContentURI): TokenId does not exist.');
-        }
-    }
-
-    /**
+  /**
    * Fetches the metadata uri for the specified media on an instance of the ZAP Media Contract
    * @param mediaId
    */
-    public async fetchMetadataURI(mediaId: BigNumberish): Promise<string> {
-        return this.contract.tokenMetadataURI(mediaId)
+  public async fetchMetadataURI(mediaId: BigNumberish): Promise<string> {
+    return this.contract.tokenMetadataURI(mediaId);
+  }
+
+  /**
+   * Fetches the current bid shares for the specified media on an instance of the Zora Media Contract
+   * @param mediaId
+   */
+  //   public async fetchCurrentBidShares(mediaId: BigNumberish): Promise<BidShares> {
+  //     return this.market.bidSharesForToken(mediaId);
+  //   }
+
+  /**
+   * Fetches the total amount of non-burned media that has been minted on an instance of the Zap Media Contract
+   */
+  public async fetchTotalMedia(): Promise<BigNumber> {
+    return this.contract.totalSupply();
+  }
+
+  /**
+   * Mints a new piece of media on an instance of the Zap Media Contract
+   * @param mintData
+   * @param bidShares
+   */
+  public async mint(mediaData: MediaData, bidShares: BidShares): Promise<any> {
+    try {
+      validateURI(mediaData.tokenURI);
+      validateURI(mediaData.metadataURI);
+      validateBidShares(bidShares.collabShares, bidShares.creator, bidShares.owner);
+    } catch (err: any) {
+      return Promise.reject(err.message);
     }
 
-    /**
-     * Mints a new piece of media on an instance of the Zora Media Contract
-     * @param mintData
-     * @param bidShares
-     */
-    public async mint(mediaData: MediaData, bidShares: BidShares): Promise<any> {
-        try {
-            validateURI(mediaData.tokenURI);
-            validateURI(mediaData.metadataURI);
-            validateBidShares(bidShares.collabShares, bidShares.creator, bidShares.owner);
-        } catch (err: any) {
-            return Promise.reject(err.message);
-        }
+    const gasEstimate = await this.contract.estimateGas.mint(mediaData, bidShares);
 
-        const gasEstimate = await this.contract.estimateGas.mint(mediaData, bidShares);
+    return this.contract.mint(mediaData, bidShares, { gasLimit: gasEstimate });
+  }
 
-        return await this.contract.mint(mediaData, bidShares, { gasLimit: gasEstimate });
+  public async updateContentURI(mediaId: number, tokenURI: string): Promise<ContractTransaction> {
+    try {
+      return await this.contract.updateTokenURI(mediaId, tokenURI);
+    } catch (err) {
+      invariant(false, 'ZapMedia (updateContentURI): TokenId does not exist.');
     }
+  }
 
-    public async updateContentURI(mediaId: number, tokenURI: string): Promise<ContractTransaction> {
-        try {
-            return await this.contract.updateTokenURI(mediaId, tokenURI);
-        } catch (err) {
-            invariant(false, 'ZapMedia (updateContentURI): TokenId does not exist.');
-        }
-    }
-
-    /**
-   * Updates the metadata uri for the specified media on an instance of the Zora Media Contract
+  /**
+   * Updates the metadata uri for the specified media on an instance of the Zap Media Contract
    * @param mediaId
    * @param metadataURI
    */
-    public async updateMetadataURI(
-        mediaId: BigNumberish,
-        metadataURI: string
-    ): Promise<ContractTransaction> {
-        try {
-            validateURI(metadataURI)
-        } catch (err: any) {
-            return Promise.reject(err.message)
-        }
-
-        return this.contract.updateTokenMetadataURI(mediaId, metadataURI)
+  public async updateMetadataURI(
+    mediaId: BigNumberish,
+    metadataURI: string,
+  ): Promise<ContractTransaction> {
+    try {
+      validateURI(metadataURI);
+    } catch (err: any) {
+      return Promise.reject(err.message);
     }
 
-
-    public async fetchBalanceOf(owner: string): Promise<BigNumber> {
-        return this.contract.balanceOf(owner);
-    }
+    return this.contract.updateTokenMetadataURI(mediaId, metadataURI);
+  }
 }
 export default ZapMedia;
