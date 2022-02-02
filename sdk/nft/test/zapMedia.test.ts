51dbcb391e34d5e5c21471182cc9f90f1234d3b5
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2022-02-01 22:37:39 -0500

    Change describe block name

--- sdk/nft/test/zapMedia.test.ts
+++ sdk/nft/test/zapMedia.test.ts
@@ -1009,7 +1009,7 @@ describe("ZapMedia", () => {
           expect(parseInt(bidderPostBal._hex)).to.equal(600);
         });
 
-        describe("#bidForTokenBidder", () => {
+        describe("#fetchCurrentBidForBidder", () => {
           it("Should reject if the media contract is a zero address", async () => {
             await ownerConnected
               .fetchCurrentBidForBidder(
