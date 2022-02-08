4240c9fc66986aa068669dc8f4f4b5a7f0b4d9e4
cryptichic <syberchief@gmail.com>
2022-02-08 13:03:22 -0600

    should reject is the bid currency is a zero address on the main media test wasn't passing; fixed

--- sdk/nft/test/zapMedia.test.ts
+++ sdk/nft/test/zapMedia.test.ts
@@ -1007,7 +1007,7 @@ describe("ZapMedia", () => {
         });
       });
 
-      describe.only("#setbid", () => {
+      describe("#setbid", () => {
         let bidder: Signer;
         let bid: Bid;
         let bidderMainConnected: ZapMedia;
@@ -1055,7 +1055,7 @@ describe("ZapMedia", () => {
             );
         });
 
-        it("Should reject if the bid currency is a zero address on the main media", async () => {
+        it.only("Should reject if the bid currency is a zero address on the main media", async () => {
           // The bidder approves zapMarket to receive the bid amount before setting the bid
           await token.connect(bidder).approve(zapMarket.address, bid.amount);
 
@@ -1065,7 +1065,7 @@ describe("ZapMedia", () => {
           // The bidder attempts to set a bid with the currency as a zero address
           await bidderMainConnected
             .setBid(0, bid)
-            .should.be.rejected(
+            .should.be.rejectedWith(
               "Invariant failed: ZapMedia (setBid): Currency cannot be a zero address."
             );
         });
@@ -1124,6 +1124,21 @@ describe("ZapMedia", () => {
 
           // The bidder attempts to set a bid with zero tokens
           await bidderMainConnected
+            .setBid(0, bid)
+            .should.be.rejectedWith(
+              "Invariant failed: ZapMedia (setBid): Amount cannot be zero."
+            );
+        });
+
+        it("Should reject if the bid amount is zero on a custom media", async () => {
+          // The bidder approves zapMarket to receive the bid amount before setting the bid
+          await token.connect(bidder).approve(zapMarket.address, bid.amount);
+
+          // Sets the bid amount to zero
+          bid.amount = 0;
+
+          // The bidder attempts to set a bid with zero tokens
+          await bidderCustomConnected
             .setBid(0, bid)
             .should.be.rejectedWith(
               "Invariant failed: ZapMedia (setBid): Amount cannot be zero."
