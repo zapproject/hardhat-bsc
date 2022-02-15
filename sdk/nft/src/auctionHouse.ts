f8aee37cb8a6a7e17617b9e30eb9cc0d3adb1a74
WesMiner99 <wabailif@uark.edu>
2022-02-15 12:51:34 -0600

    does auctionId exist

--- sdk/nft/src/auctionHouse.ts
+++ sdk/nft/src/auctionHouse.ts
@@ -234,9 +234,15 @@ class AuctionHouse {
   }
 
   public async cancelAuction(auctionId: BigNumberish) {
-    // Step 1 - Figure out a way to check if an auction id exists or not. If it doesnt exist
-    // throw an error
-    return this.auctionHouse.cancelAuction(auctionId);
+    const auctionInfo = await this.fetchAuction(auctionId);
+
+    if (auctionInfo.token.mediaContract == ethers.constants.AddressZero) {
+      invariant(false, "If auction Id does not exist");
+    } else {
+      // Step 1 - Figure out a way to check if an auction id exists or not. If it doesnt exist
+      // throw an error
+      return this.auctionHouse.cancelAuction(auctionId);
+    }
   }
 }
 
