422f2afd498eefdadef1229928f6fba1478847a2
Harris Levine <acyclicgraphs@gmail.com>
2022-03-05 18:08:31 -0500

    Altered tests

--- zswap/contracts/ZswapStake.sol
+++ zswap/contracts/ZswapStake.sol
@@ -6,46 +6,46 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
-// You come in with some GZap, and leave with more! The longer you stay, the more GZap you get.
+// You come in with some $Zap, and leave with more! The longer you stay, the more Zap you get.
 //
 // This contract handles swapping to and from xZap, ZSwap's staking token.
 contract ZswapStake is ERC20("ZswapStake", "xZap"){
     using SafeMath for uint256;
-    IERC20 public sushi;
+    IERC20 public zap;
 
-    // Define the Sushi token contract
-    constructor(IERC20 gzap) public {
-        gzap = _gzap;
+    // Define the Zap token contract
+    constructor(IERC20 zap) public {
+        zap = _zap;
     }
 
-    // Enter the bar. Pay some SUSHIs. Earn some shares.
-    // Locks Sushi and mints xSushi
+    // Enter. Pay some Zaps. Earn some shares.
+    // Locks Zap and mints xZap
     function enter(uint256 _amount) public {
-        // Gets the amount of Sushi locked in the contract
-        uint256 totalGZap = gzap.balanceOf(address(this));
-        // Gets the amount of xSushi in existence
+        // Gets the amount of Zap locked in the contract
+        uint256 totalZap = zap.balanceOf(address(this));
+        // Gets the amount of xZap in existence
         uint256 totalShares = totalSupply();
-        // If no xSushi exists, mint it 1:1 to the amount put in
+        // If no xZap exists, mint it 1:1 to the amount put in
         if (totalShares == 0 || totalSushi == 0) {
             _mint(msg.sender, _amount);
         } 
-        // Calculate and mint the amount of xSushi the Sushi is worth. The ratio will change overtime, as xSushi is burned/minted and Sushi deposited + gained from fees / withdrawn.
+        // Calculate and mint the amount of xZap the Zap is worth. The ratio will change overtime, as xZap is burned/minted and Zap deposited + gained from fees / withdrawn.
         else {
             uint256 what = _amount.mul(totalShares).div(totalSushi);
             _mint(msg.sender, what);
         }
-        // Lock the Sushi in the contract
-        sushi.transferFrom(msg.sender, address(this), _amount);
+        // Lock the Zap in the contract
+        zap.transferFrom(msg.sender, address(this), _amount);
     }
 
-    // Leave the bar. Claim back your SUSHIs.
-    // Unlocks the staked + gained Sushi and burns xSushi
+    // Leave the "bar". Claim back your Zaps.
+    // Unlocks the staked + gained Zap and burns xZap
     function leave(uint256 _share) public {
-        // Gets the amount of xSushi in existence
+        // Gets the amount of xZap in existence
         uint256 totalShares = totalSupply();
-        // Calculates the amount of Sushi the xSushi is worth
-        uint256 what = _share.mul(sushi.balanceOf(address(this))).div(totalShares);
+        // Calculates the amount of Zap the xZap is worth
+        uint256 what = _share.mul(zap.balanceOf(address(this))).div(totalShares);
         _burn(msg.sender, _share);
-        gzap.transfer(msg.sender, what);
+        zap.transfer(msg.sender, what);
     }
 }
