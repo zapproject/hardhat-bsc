a04714bd6661c2e4bdd5d878d541c6ece87573a5
Harris Levine <acyclicgraphs@gmail.com>
2022-03-05 17:48:14 -0500

    Continued

--- zswap/contracts/ZswapStake.sol
+++ zswap/contracts/ZswapStake.sol
@@ -6,23 +6,23 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
-// SushiBar is the coolest bar in town. You come in with some Sushi, and leave with more! The longer you stay, the more Sushi you get.
+// You come in with some GZap, and leave with more! The longer you stay, the more GZap you get.
 //
-// This contract handles swapping to and from xSushi, SushiSwap's staking token.
-contract SushiBar is ERC20("SushiBar", "xSUSHI"){
+// This contract handles swapping to and from xZap, ZSwap's staking token.
+contract ZswapStake is ERC20("ZswapStake", "xZap"){
     using SafeMath for uint256;
     IERC20 public sushi;
 
     // Define the Sushi token contract
-    constructor(IERC20 _sushi) public {
-        sushi = _sushi;
+    constructor(IERC20 gzap) public {
+        gzap = _gzap;
     }
 
     // Enter the bar. Pay some SUSHIs. Earn some shares.
     // Locks Sushi and mints xSushi
     function enter(uint256 _amount) public {
         // Gets the amount of Sushi locked in the contract
-        uint256 totalSushi = sushi.balanceOf(address(this));
+        uint256 totalGZap = gzap.balanceOf(address(this));
         // Gets the amount of xSushi in existence
         uint256 totalShares = totalSupply();
         // If no xSushi exists, mint it 1:1 to the amount put in
@@ -46,6 +46,6 @@ contract SushiBar is ERC20("SushiBar", "xSUSHI"){
         // Calculates the amount of Sushi the xSushi is worth
         uint256 what = _share.mul(sushi.balanceOf(address(this))).div(totalShares);
         _burn(msg.sender, _share);
-        sushi.transfer(msg.sender, what);
+        gzap.transfer(msg.sender, what);
     }
 }
