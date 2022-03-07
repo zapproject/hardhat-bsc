1dccd46dfa60f55974c8c82d096699481d4b2b6a
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2022-03-07 09:37:20 -0500

    ZapMarketV2 intialize section passing

--- hardhat/test/ZapMarketV2.test.ts
+++ hardhat/test/ZapMarketV2.test.ts
@@ -9,7 +9,8 @@ import {
   Media1155,
   ZapMarket,
   ZapMarketV2,
-  ZapVault
+  ZapVault,
+  ZapMedia
 } from '../typechain';
 
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
@@ -22,6 +23,9 @@ describe('ZapMarketV2', () => {
   let zapMarket: ZapMarket;
   let zapMarketV2: ZapMarketV2;
   let zapVault: ZapVault;
+  let media1155Factory: Media1155Factory;
+  let zapMedia: ZapMedia;
+  let media1155: Media1155;
 
   beforeEach(async () => {
     signers = await ethers.getSigners();
@@ -65,6 +69,48 @@ describe('ZapMarketV2', () => {
       zapMarketV2Address,
       signers[0]
     )) as ZapMarketV2;
+
+    // Deploy the Media1155 implementation through hardhat-deploy
+    const deployMedia1155ImpTx: DeployResult = await deployments.deploy(
+      'Media1155',
+      {
+        from: signers[0].address,
+        args: []
+      }
+    );
+
+    // Fetch the address of Media1155 implementation from the transaction receipt
+    const media1155ImpAddress: string | any =
+      deployMedia1155ImpTx.receipt?.contractAddress;
+
+    // Deploy the Media1155Factory through hardhat-deploy
+    const deployMedia1155Factory: DeployResult = await deployments.deploy(
+      'Media1155Factory',
+      {
+        from: signers[0].address,
+        proxy: {
+          proxyContract: 'OpenZeppelinTransparentProxy',
+          execute: {
+            methodName: 'initialize',
+            args: [zapMarketV2.address, media1155ImpAddress]
+          }
+        }
+      }
+    );
+
+    // Fetch the Media1155Factory address from the transaction receipt
+    const media1155FactoryAddress: string | any =
+      deployMedia1155Factory.receipt?.contractAddress;
+
+    // Creates the Media1155 contract instance
+    media1155Factory = (await ethers.getContractAt(
+      'Media1155Factory',
+      media1155FactoryAddress,
+      signers[0]
+    )) as Media1155Factory;
+
+    // ZapMarketV2 sets the Media1155Factory
+    await zapMarketV2.setMediaFactory(media1155Factory.address);
   });
 
   describe.only('#intitialize', () => {
@@ -83,8 +129,30 @@ describe('ZapMarketV2', () => {
     it('Should get the the platform fee', async () => {
       const fee = await zapMarketV2.viewFee();
       expect(parseInt(fee.value._hex)).to.equal(5e18);
+    });
 
-      console.log(zapMarket.address == zapMarketV2.address);
+    it('Should revert if a non owner tries to set the plaform fee', async () => {
+      let platformFee = {
+        fee: {
+          value: ethers.BigNumber.from('5000000000000000000')
+        }
+      };
+      await expect(
+        zapMarketV2.connect(signers[2]).setFee(platformFee)
+      ).to.be.revertedWith('Ownable: Only owner has access to this function');
+    });
+
+    it('Should revert if configure is not called by the media factory', async () => {
+      await expect(
+        zapMarketV2.configure(
+          signers[0].address,
+          (
+            await deployments.get('ZapMedia')
+          ).address,
+          ethers.utils.formatBytes32String('Test Media'),
+          ethers.utils.formatBytes32String('TM')
+        )
+      ).to.be.revertedWith('Market: Only the media factory can do this action');
     });
   });
 });
