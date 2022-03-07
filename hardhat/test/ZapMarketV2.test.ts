c547646529f5c65b3756ae4bb246d05ef9d18030
Kimani Kelly <kimanikelly@Kimanis-MacBook-Pro.local>
2022-03-07 13:53:25 -0500

    Added the 'Should return null values if the token id does not exsist' test

--- hardhat/test/ZapMarketV2.test.ts
+++ hardhat/test/ZapMarketV2.test.ts
@@ -29,6 +29,9 @@ describe.only('ZapMarketV2', () => {
   let media1155Factory: Media1155Factory;
   let zapMedia: ZapMedia;
   let media1155: Media1155;
+  let zapMediaAddress: string;
+  let bidShares: any;
+  let data: any;
 
   beforeEach(async () => {
     signers = await ethers.getSigners();
@@ -115,11 +118,56 @@ describe.only('ZapMarketV2', () => {
     // ZapMarketV2 sets the Media1155Factory
     await zapMarketV2.setMediaFactory(media1155Factory.address);
 
+    const zapMediaAddress = await zapMarketV2.mediaContracts(
+      signers[0].address,
+      ethers.BigNumber.from('0')
+    );
+
     zapMedia = (await ethers.getContractAt(
-      'Media1155Factory',
-      media1155FactoryAddress,
+      zapMediaAbi.abi,
+      zapMediaAddress,
       signers[0]
     )) as ZapMedia;
+
+    let tokenURI = 'www.example.com';
+    let metadataURI = 'www.example2.com';
+
+    let metadataHex = ethers.utils.formatBytes32String('{}');
+    let metadataHashRaw = ethers.utils.keccak256(metadataHex);
+    let metadataHashBytes = ethers.utils.arrayify(metadataHashRaw);
+
+    let contentHex = ethers.utils.formatBytes32String('invert');
+    let contentHashRaw = ethers.utils.keccak256(contentHex);
+    let contentHashBytes = ethers.utils.arrayify(contentHashRaw);
+
+    let contentHash = contentHashBytes;
+    let metadataHash = metadataHashBytes;
+
+    data = {
+      tokenURI,
+      metadataURI,
+      contentHash,
+      metadataHash
+    };
+
+    bidShares = {
+      collaborators: [
+        signers[17].address,
+        signers[18].address,
+        signers[19].address
+      ],
+      collabShares: [
+        ethers.BigNumber.from('15000000000000000000'),
+        ethers.BigNumber.from('15000000000000000000'),
+        ethers.BigNumber.from('15000000000000000000')
+      ],
+      creator: {
+        value: ethers.BigNumber.from('15000000000000000000')
+      },
+      owner: {
+        value: ethers.BigNumber.from('35000000000000000000')
+      }
+    };
   });
 
   describe('#intitialize', () => {
@@ -127,17 +175,6 @@ describe.only('ZapMarketV2', () => {
       await expect(
         zapMarketV2.initializeMarket(zapVault.address)
       ).to.be.revertedWith('Initializable: contract is already initialized');
-    });
-
-    it('Should get the market owner', async () => {
-      const owner = await zapMarketV2.getOwner();
-
-      expect(owner).to.equal(signers[0].address);
-    });
-
-    it('Should get the the platform fee', async () => {
-      const fee = await zapMarketV2.viewFee();
-      expect(parseInt(fee.value._hex)).to.equal(5e18);
     });
 
     it('Should revert if a non owner tries to set the plaform fee', async () => {
@@ -152,11 +189,6 @@ describe.only('ZapMarketV2', () => {
     });
 
     it('Should get an owners media contract', async () => {
-      const zapMediaAddress = await zapMarketV2.mediaContracts(
-        signers[0].address,
-        ethers.BigNumber.from('0')
-      );
-
       const registerStatus = await zapMarketV2.isRegistered(zapMediaAddress);
 
       const configureStatus = await zapMarketV2.isConfigured(zapMediaAddress);
@@ -164,6 +196,17 @@ describe.only('ZapMarketV2', () => {
       expect(registerStatus).to.be.true;
 
       expect(configureStatus).to.be.true;
+
+      it('Should get the market owner', async () => {
+        const owner = await zapMarketV2.getOwner();
+
+        expect(owner).to.equal(signers[0].address);
+      });
+
+      it('Should get the the platform fee', async () => {
+        const fee = await zapMarketV2.viewFee();
+        expect(parseInt(fee.value._hex)).to.equal(5e18);
+      });
     });
 
     it('Should revert if configure is not called by the media factory', async () => {
@@ -177,6 +220,36 @@ describe.only('ZapMarketV2', () => {
           ethers.utils.formatBytes32String('TM')
         )
       ).to.be.revertedWith('Market: Only the media factory can do this action');
+    });
+  });
+
+  describe.only('bidShareForToken', () => {
+    beforeEach(async () => {
+      await zapMedia.mint(data, bidShares);
+    });
+
+    it('Should return null values if the token id does not exsist', async () => {
+      const fetchBidShares = await zapMarketV2.bidSharesForToken(
+        zapMedia.address,
+        1
+      );
+
+      expect(fetchBidShares.collaborators).to.have.lengthOf(0);
+      expect(fetchBidShares.collabShares).to.have.lengthOf(0);
+      expect(fetchBidShares.owner.value.toNumber()).to.equal(0);
+      expect(fetchBidShares.creator.value.toNumber()).to.equal(0);
+    });
+
+    it('Should return the bid shares for a token', async () => {
+      const fetchBidShares = await zapMarketV2.bidSharesForToken(
+        zapMedia.address,
+        0
+      );
+
+      expect(fetchBidShares.collaborators).to.eql(bidShares.collaborators);
+      expect(fetchBidShares.collabShares).to.eql(bidShares.collabShares);
+      expect(fetchBidShares.creator.value).to.equal(bidShares.creator.value);
+      expect(fetchBidShares.owner.value).to.equal(bidShares.owner.value);
     });
   });
 });
